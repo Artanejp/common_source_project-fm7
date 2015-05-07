3e9a30e8a1a9a4c67b2135d87b0cc39672f56eb8
K.Ohta <whatisthis.sowhat@gmail.com>
2015-05-07 19:44:00 +0900

    [BUILD][CMAKE][BATCH] Stop building when compilation error(s).

--- source/build-cmake/config_build.sh
+++ source/build-cmake/config_build.sh
@@ -9,23 +9,25 @@ MAKEFLAGS_CC="-g -O3 -DNDEBUG"
 BUILD_TYPE="Relwithdebinfo"
 CMAKE_APPENDFLAG=""
 
-. ./buildvars.dat
+if [ -e ./buildvars.dat ] ; then
+    . ./buildvars.dat
+fi
 
 case ${BUILD_TYPE} in
     "Debug" | "DEBUG" | "debug" ) 
-            CMAKE_FLAGS1="-D CMAKE_BUILD_TYPE:STRING=\"debug\""
-	    CMAKE_FLAGS2="-D CMAKE_CXX_FLAGS_DEBUG"
-	    CMAKE_FLAGS3="-D CMAKE_C_FLAGS_DEBUG"
+            CMAKE_FLAGS1="-DCMAKE_BUILD_TYPE:STRING=debug"
+	    CMAKE_FLAGS2="-DCMAKE_CXX_FLAGS_DEBUG:STRING"
+	    CMAKE_FLAGS3="-DCMAKE_C_FLAGS_DEBUG:STRING"
 	    ;;
     "Release" | "RELEASE" | "release" ) 
-            CMAKE_FLAGS1="-D CMAKE_BUILD_TYPE:STRING=\"release\""
-	    CMAKE_FLAGS2="-D CMAKE_CXX_FLAGS_RELEASE"
-	    CMAKE_FLAGS3="-D CMAKE_C_FLAGS_RELEASE"
+            CMAKE_FLAGS1="-DCMAKE_BUILD_TYPE:STRING=Release"
+	    CMAKE_FLAGS2="-DCMAKE_CXX_FLAGS_RELEASE:STRING"
+	    CMAKE_FLAGS3="-DCMAKE_C_FLAGS_RELEASE:STRING"
 	    ;;
     "Relwithdebinfo" | "RELWITHDEBINFO" | "relwithdebinfo" ) 
-            CMAKE_FLAGS1="-D CMAKE_BUILD_TYPE:STRING=\"Relwithdebinfo\""
-	    CMAKE_FLAGS2="-D CMAKE_CXX_FLAGS_RELWITHDEBINFO"
-	    CMAKE_FLAGS3="-D CMAKE_C_FLAGS_RELWITHDEBINFO"
+            CMAKE_FLAGS1="-DCMAKE_BUILD_TYPE:STRING=Relwithdebinfo"
+	    CMAKE_FLAGS2="-DCMAKE_CXX_FLAGS_RELWITHDEBINFO:STRING"
+	    CMAKE_FLAGS3="-DCMAKE_C_FLAGS_RELWITHDEBINFO:STRING"
 	    ;;
      * )
             echo "Specify BUILD_TYPE in buildvars.dat to Debug, Release, Relwithdebinfo."
@@ -41,25 +43,26 @@ for SRCDATA in $@ ; do\
     echo ${CMAKE_FLAGS1} ${CMAKE_FLAGS2}
     ${CMAKE} -D CMAKE_C_COMPILER:STRING=${CCMAKE_CC}  \
              -D CMAKE_CXX_COMPILER:STRING=${CCMAKE_CXX} \
-	     "${CMAKE_FLAGS1}" \
-	     "${CMAKE_FLAGS2}=\"${MAKEFLAGS_CXX}\"" \
-	     "${CMAKE_FLAGS3}=\"${MAKEFLAGS_CC}\"" \
-	     "${CMAKE_APPENDFLAG}" \
-	     ..
+	     ${CMAKE_FLAGS1} \
+	     "${CMAKE_FLAGS2}=${MAKEFLAGS_CXX}" \
+	     "${CMAKE_FLAGS3}=${MAKEFLAGS_CC}" \
+	     ${CMAKE_APPENDFLAG} \
+	     .. | tee make.log
 
     ${CMAKE} -D CMAKE_C_COMPILER:STRING=${CCMAKE_CC}  \
              -D CMAKE_CXX_COMPILER:STRING=${CCMAKE_CXX} \
-	     "${CMAKE_FLAGS1}" \
-	     "${CMAKE_FLAGS2}=\"${MAKEFLAGS_CXX}\"" \
-	     "${CMAKE_FLAGS3}=\"${MAKEFLAGS_CC}\"" \
-	     "${CMAKE_APPENDFLAG}" \
-	     ..
+	     ${CMAKE_FLAGS1} \
+	     "${CMAKE_FLAGS2}=${MAKEFLAGS_CXX}" \
+	     "${CMAKE_FLAGS3}=${MAKEFLAGS_CC}" \
+	     ${CMAKE_APPENDFLAG} \
+	     .. | tee -a make.log
 
     make clean
     make -j12 2>&1 | tee -a ./make.log
-    
-    sudo make install
-    
+    case $? in
+      0 ) sudo make install ;;
+      * ) exit $? ;;
+    esac
     cd ../..
 done
 
