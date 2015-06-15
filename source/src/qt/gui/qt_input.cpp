db8a131900df634b2fcc66aa280396fcb24943bb
K.Ohta <whatisthis.sowhat@gmail.com>
2015-06-16 05:03:23 +0900

    [UI][Qt] WIP:

--- source/src/qt/gui/qt_input.cpp
+++ source/src/qt/gui/qt_input.cpp
@@ -40,6 +40,11 @@ struct NativeScanCode {
 	uint32 vk;
 	uint32 scan;
 };
+struct NativeVirtualKeyCode {
+	uint32 vk;
+	uint32 key;
+};
+
 struct NativeScanCode convTable_QTScan106[] = {
 	// Line 0
 	{VK_ESCAPE,  9},
@@ -162,7 +167,168 @@ struct NativeScanCode convTable_QTScan106[] = {
 	{VK_RETURN, 104},  // Enter(ten Key)
 	{0xffffffff, 0xffffffff}
 };
-	
+#if defined(Q_OS_WIN32)
+#else // Linux or Unix
+const struct NativeVirtualKeyCode convTable_QTKey[] = {
+	// Line 0
+	{VK_ESCAPE,  0xff1b},
+	{VK_F1,  0xffbe},
+	{VK_F2,  0xffbf},
+	{VK_F3,  0xffc0},
+	{VK_F4,  0xffc1},
+	{VK_F5,  0xffc2},
+	{VK_F6,  0xffc3},
+	{VK_F7,  0xffc4},
+	{VK_F8,  0xffc5},
+	{VK_F9,  0xffc6},
+	{VK_F10, 0xffc7},
+	{VK_F11, 0xffc8},
+	{VK_F12, 0xffc9},
+	// Power, Sleep, Wake is not implemented, they are'nt safety.
+	// Line 1
+	{VK_KANJI, 0xff2a}, // Hankaku/Zenkaku
+	{0xbd, 0x002d}, // -=
+	{0xbd, 0x003d},
+	{0xde, 0x005e}, // ^~
+	{0xde, 0x007e},
+	{0xdc, 0x005c}, // \|
+	{0xdc, 0x007c},
+	{VK_BACK, 0xff08}, // Backspace
+	// Line 2
+	{VK_TAB, 0xff09},
+	{VK_RETURN, 0xff0d}, // Enter (Full key)
+	{0xdb, 0x005b}, // [
+	{0xdb, 0x007b}, // [
+	// Line 3
+	{0xf0, 0xff30}, // CAPS Lock
+	{0xbb, 0x002b}, // ;
+	{0xbb, 0x003b}, // ;
+	{0xba, 0x002a}, // :
+	{0xba, 0x003a}, // :
+	{0xdd, 0x005d}, // ]
+	{0xdd, 0x007d}, // ]
+	// Line 3
+	{VK_LSHIFT, 0xffe1}, // LShift
+	{0xbc, 0x2c}, // ,
+	{0xbc, 0x3c}, // ,
+	{0xbe, 0x2e}, // .
+	{0xbe, 0x3e}, // .
+	{0xbf, 0x2f}, // /(Slash)
+	{0xbf, 0x3f}, // /(Slash)
+	{0xe2, 0x5f}, //\_
+	//{0xe2, 0x5c}, //\_
+	{VK_RSHIFT, 0xffe2},
+	// Line 4
+	{VK_LCONTROL, 0xffe3},
+	{VK_LWIN, 0xffeb},
+	{VK_LMENU, 0xffe9},
+	{0x1d, 0xff22}, // Muhenkan
+	{VK_SPACE, 0x0020},
+	{0xf3, 0xff23}, // Henkan
+	{0xf2, 0xff27}, // Katakana_Hiragana
+	{VK_RMENU, 0xffea},
+	{VK_RWIN,  0xffec},
+	{VK_APPS, 0xff67},
+	{VK_RCONTROL, 0xffe4},
+	// Cursors
+	{VK_UP, 0xff52},
+	{VK_DOWN, 0xff54},
+	{VK_LEFT, 0xff51},
+	{VK_RIGHT,0xff53},
+	// 
+	//     {VK_PRINT, },
+	{VK_SCROLL, 0xff14},
+	{VK_PAUSE, 0xff13},
+	{VK_INSERT, 0xff63},
+	{VK_HOME, 0xff50},
+	{VK_NEXT, 0xff55},
+	{VK_DELETE, 0xffff},
+	{VK_END, 0xff57},
+	{VK_PRIOR, 0xff56},
+	// TenKey
+	{VK_NUMPAD0, 0xffb0},
+	{VK_NUMPAD1, 0xffb1},
+	{VK_NUMPAD2, 0xffb2},
+	{VK_NUMPAD3, 0xffb3},
+	{VK_NUMPAD4, 0xffb4},
+	{VK_NUMPAD5, 0xffb5},
+	{VK_NUMPAD6, 0xffb6},
+	{VK_NUMPAD7, 0xffb7},
+	{VK_NUMPAD8, 0xffb8},
+	{VK_NUMPAD9, 0xffb9},
+	//
+	{VK_DECIMAL, 0xff7f}, // NumLock     
+	{VK_DIVIDE, 0xffaf},
+	{VK_MULTIPLY, 0xffaa},
+	{VK_SUBTRACT, 0xffad},
+	{VK_ADD, 0xffab},
+	{VK_RETURN, 0xff8d},  // Enter(ten Key)
+	{VK_DECIMAL, 0xffae},  // Period(ten Key)
+	{0xffffffff, 0xffffffff}
+};
+#endif
+
+#if defined(Q_OS_WIN32)
+uint32_t GLDrawClass::getNativeKey2VK(uint32_t data)
+{
+	uint32_t vk = data;
+#if defined(ENABLE_SWAP_KANJI_PAUSE)
+	if(config.swap_kanji_pause) {
+		if(vk == VK_KANJI) {
+			vk = VK_PAUSE;
+		} else if(vk == VK_PAUSE) {
+			vk = VK_KANJI;
+		}
+	}
+#endif	   
+	return vk;
+}
+#else
+uint32_t GLDrawClass::getNativeKey2VK(uint32_t data)
+{
+	uint32 val = 0;
+	uint32 vk;
+	int i = 0;
+
+	if(data < 0x100) {
+		if((data >= 0x60) && (data <= 'z')) {
+	  		return data & 0x5f;
+		}
+		if((data >= 0x40) && (data <= 'Z')) {
+	  		return data;
+		}
+		if((data >= 0x30) && (data <= 0x39)) {
+	  		return data;
+		}
+		if((data > 0x20) && (data <= 0x29)) {
+	  		return data | 0x10;
+		}
+	}
+	while(convTable_QTKey[i].vk != 0xffffffff) {
+		val = convTable_QTKey[i].key;
+		if(val == data) break;
+		i++;
+	}
+	vk = convTable_QTKey[i].vk;
+	//printf("SCAN=%02x VK=%02x\n", val, vk);
+	if(vk == 0xffffffff) return 0;
+#if defined(ENABLE_SWAP_KANJI_PAUSE)
+	if(config.swap_kanji_pause) {
+		if(vk == VK_KANJI) {
+			vk = VK_PAUSE;
+		} else if(vk == VK_PAUSE) {
+			vk = VK_KANJI;
+		}
+	}
+#endif	   
+//	if(vk == VK_KANJI) vk = 0xf3;
+	if((vk == VK_LSHIFT) || (vk == VK_RSHIFT)) vk = VK_SHIFT;
+	if((vk == VK_LCONTROL) || (vk == VK_RCONTROL)) vk = VK_CONTROL;
+	if((vk == VK_LMENU) || (vk == VK_RMENU)) vk = VK_MENU;
+	return vk;
+}
+#endif
+
 uint32_t GLDrawClass::get106Scancode2VK(uint32_t data)
 {
 	uint32 val = 0;
@@ -192,15 +358,21 @@ uint32_t GLDrawClass::get106Scancode2VK(uint32_t data)
 	return vk;
 }
 
-
 void GLDrawClass::keyReleaseEvent(QKeyEvent *event)
 {
 	int key = event->key();
 	uint32 mod = event->modifiers();
-	uint32 scan = event->nativeScanCode();
+	uint32 scan;
 	uint32 vk;
 	if(event->isAutoRepeat()) return;
+#if 0
+	scan = event->nativeScanCode();
 	vk = get106Scancode2VK(scan);
+#else
+	scan = event->nativeVirtualKey();
+	vk = getNativeKey2VK(scan);
+#endif	
+	//printf("Key: VK=%d SCAN=%04x MOD=%08x\n", vk, scan, mod);
 	emu->LockVM();
 	emu->key_mod(mod);
 	// Note: Qt4 with 106KEY, event->modifier() don't get Shift key as KEYMOD.
@@ -217,12 +389,18 @@ void GLDrawClass::keyPressEvent(QKeyEvent *event)
 {
 	int key = event->key();
 	uint32 mod = event->modifiers();;
-	uint32 scan = event->nativeScanCode();
+	uint32 scan;
 	uint32 vk;
    
 	if(event->isAutoRepeat()) return;
+#if 0
+	scan = event->nativeScanCode();
 	vk = get106Scancode2VK(scan);
-	//  printf("Key: VK=%d SCAN=%d MOD=%08x\n", vk, scan, mod);
+#else
+	scan = event->nativeVirtualKey();
+	vk = getNativeKey2VK(scan);
+#endif	
+	//printf("Key: VK=%d SCAN=%04x MOD=%08x\n", vk, scan, mod);
 	emu->LockVM();
 	emu->key_mod(mod);
 //#ifdef NOTIFY_KEY_DOWN
