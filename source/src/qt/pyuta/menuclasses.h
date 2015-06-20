d11c74baa68c734e43c753b6658d6a8eb5a31115
K.Ohta <whatisthis.sowhat@gmail.com>
2015-06-20 17:32:49 +0900

    [Qt] Add Tomy PYUTA.

---
+++ source/src/qt/pyuta/menuclasses.h
@@ -0,0 +1,27 @@
+
+#ifndef _CSP_QT_MENUCLASSES_H
+#define _CSP_QT_MENUCLASSES_H
+
+#include "mainwidget.h"
+// This extends class CSP_MainWindow as Ui_MainWindow.
+// You may use this as 
+QT_BEGIN_NAMESPACE
+
+
+class Ui_MainWindow;
+//  wrote of X1 Specific menu.
+class META_MainWindow : public Ui_MainWindow {
+  Q_OBJECT
+ protected:
+  void setupUI_Emu(void);
+  void retranslateUi(void);
+ public:
+  META_MainWindow(QWidget *parent = 0);
+  ~META_MainWindow();
+public slots:
+
+};
+
+QT_END_NAMESPACE
+
+#endif // END
