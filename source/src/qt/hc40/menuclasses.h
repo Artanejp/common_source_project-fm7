31c6ec74850c3d6e923633b39deab8db78c7fda3
K.Ohta <whatisthis.sowhat@gmail.com>
2015-06-20 16:52:27 +0900

    [VM][Qt] Add Epson HC40.

---
+++ source/src/qt/hc40/menuclasses.h
@@ -0,0 +1,32 @@
+
+
+
+#ifndef _CSP_QT_MENUCLASSES_H
+#define _CSP_QT_MENUCLASSES_H
+
+#include "emu.h"
+#include "mainwidget.h"
+// This extends class CSP_MainWindow as Ui_MainWindow.
+// You may use this as 
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow;
+
+class META_MainWindow : public Ui_MainWindow {
+  Q_OBJECT
+ protected:
+  void setupUI_Emu(void);
+  void retranslateUi(void);
+  
+  QMenu *menu_Emu_DipSw;
+  
+ public:
+  META_MainWindow(QWidget *parent = 0);
+  ~META_MainWindow();
+public slots:
+
+};
+
+QT_END_NAMESPACE
+
+#endif // END
