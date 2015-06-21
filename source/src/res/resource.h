4c31a84750fd00878156c0cdd938221057cc22ab
Kyuma Ohta <whatisthis.sowhat@gmail.com>
2015-06-21 21:27:02 +0900

    [Build][Win32] Fix wrong ICON for X1 TurboZ.

--- source/src/res/resource.h
+++ source/src/res/resource.h
@@ -1,15 +1,14 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
-// Used by resource.rc
+// Microsoft Visual C++ generated include file.
+// Used by x1turboz.rc
 //
 #define IDR_MENU1                       101
-#define IDI_ICON1                       102
 #define IDR_ACCELERATOR1                103
 #define ID_ACCEL_SCREEN                 104
 #define ID_ACCEL_MOUSE                  105
 #define ID_BITMAP1                      106
+#define IDI_ICON1                       109
 #define ID_BUTTON                       1000
-
 #define ID_RESET                        40001
 #define ID_SPECIAL_RESET                40002
 #define ID_AUTOKEY_START                40003
@@ -17,13 +16,11 @@
 #define ID_SAVE_STATE                   40005
 #define ID_LOAD_STATE                   40006
 #define ID_EXIT                         40007
-
 #define ID_OPEN_DEBUGGER0               40011
 #define ID_OPEN_DEBUGGER1               40012
 #define ID_OPEN_DEBUGGER2               40013
 #define ID_OPEN_DEBUGGER3               40014
 #define ID_CLOSE_DEBUGGER               40015
-
 #define ID_BOOT_MODE0                   40021
 #define ID_BOOT_MODE1                   40022
 #define ID_BOOT_MODE2                   40023
@@ -32,7 +29,6 @@
 #define ID_BOOT_MODE5                   40026
 #define ID_BOOT_MODE6                   40027
 #define ID_BOOT_MODE7                   40028
-
 #define ID_CPU_TYPE0                    40031
 #define ID_CPU_TYPE1                    40032
 #define ID_CPU_TYPE2                    40033
@@ -41,13 +37,11 @@
 #define ID_CPU_TYPE5                    40036
 #define ID_CPU_TYPE6                    40037
 #define ID_CPU_TYPE7                    40038
-
 #define ID_CPU_POWER0                   40041
 #define ID_CPU_POWER1                   40042
 #define ID_CPU_POWER2                   40043
 #define ID_CPU_POWER3                   40044
 #define ID_CPU_POWER4                   40045
-
 #define ID_DIPSWITCH0                   40051
 #define ID_DIPSWITCH1                   40052
 #define ID_DIPSWITCH2                   40053
@@ -80,81 +74,66 @@
 #define ID_DIPSWITCH29                  40080
 #define ID_DIPSWITCH30                  40081
 #define ID_DIPSWITCH31                  40082
-
 #define ID_DEVICE_TYPE0                 40091
 #define ID_DEVICE_TYPE1                 40092
 #define ID_DEVICE_TYPE2                 40093
 #define ID_DEVICE_TYPE3                 40094
-
 #define ID_DRIVE_TYPE0                  40095
 #define ID_DRIVE_TYPE1                  40096
 #define ID_DRIVE_TYPE2                  40097
 #define ID_DRIVE_TYPE3                  40098
-
 #define ID_OPEN_FD1                     40101
 #define ID_CLOSE_FD1                    40102
-#define ID_RECENT_FD1                   40111 // 40111-40118
+#define ID_RECENT_FD1                   40111
 #define ID_D88_FILE_PATH1               40120
-#define ID_SELECT_D88_BANK1             40121 // 40121-40200
-
+#define ID_SELECT_D88_BANK1             40121
 #define ID_OPEN_FD2                     40201
 #define ID_CLOSE_FD2                    40202
-#define ID_RECENT_FD2                   40211 // 40211-40218
+#define ID_RECENT_FD2                   40211
 #define ID_D88_FILE_PATH2               40220
-#define ID_SELECT_D88_BANK2             40221 // 40221-40300
-
+#define ID_SELECT_D88_BANK2             40221
 #define ID_OPEN_FD3                     40301
 #define ID_CLOSE_FD3                    40302
-#define ID_RECENT_FD3                   40311 // 40311-40318
+#define ID_RECENT_FD3                   40311
 #define ID_D88_FILE_PATH3               40320
-#define ID_SELECT_D88_BANK3             40321 // 40321-40400
-
+#define ID_SELECT_D88_BANK3             40321
 #define ID_OPEN_FD4                     40401
 #define ID_CLOSE_FD4                    40402
-#define ID_RECENT_FD4                   40411 // 40411-40418
+#define ID_RECENT_FD4                   40411
 #define ID_D88_FILE_PATH4               40420
-#define ID_SELECT_D88_BANK4             40421 // 40421-40500
-
+#define ID_SELECT_D88_BANK4             40421
 #define ID_OPEN_FD5                     40501
 #define ID_CLOSE_FD5                    40502
-#define ID_RECENT_FD5                   40511 // 40511-40518
+#define ID_RECENT_FD5                   40511
 #define ID_D88_FILE_PATH5               40520
-#define ID_SELECT_D88_BANK5             40521 // 40521-40600
-
+#define ID_SELECT_D88_BANK5             40521
 #define ID_OPEN_FD6                     40601
 #define ID_CLOSE_FD6                    40602
-#define ID_RECENT_FD6                   40611 // 40611-40618
+#define ID_RECENT_FD6                   40611
 #define ID_D88_FILE_PATH6               40620
-#define ID_SELECT_D88_BANK6             40621 // 40621-40700
-
+#define ID_SELECT_D88_BANK6             40621
 #define ID_OPEN_FD7                     40701
 #define ID_CLOSE_FD7                    40702
-#define ID_RECENT_FD7                   40711 // 40711-40718
+#define ID_RECENT_FD7                   40711
 #define ID_D88_FILE_PATH7               40720
-#define ID_SELECT_D88_BANK7             40721 // 40721-40800
-
+#define ID_SELECT_D88_BANK7             40721
 #define ID_OPEN_FD8                     40801
 #define ID_CLOSE_FD8                    40802
-#define ID_RECENT_FD8                   40811 // 40811-40818
+#define ID_RECENT_FD8                   40811
 #define ID_D88_FILE_PATH8               40820
-#define ID_SELECT_D88_BANK8             40821 // 40821-40900
-
+#define ID_SELECT_D88_BANK8             40821
 #define ID_OPEN_QD1                     40901
 #define ID_CLOSE_QD1                    40902
-#define ID_RECENT_QD1                   40903 // 40903-40910
-
+#define ID_RECENT_QD1                   40903
 #define ID_OPEN_QD2                     40911
 #define ID_CLOSE_QD2                    40912
-#define ID_RECENT_QD2                   40913 // 40913-40920
-
+#define ID_RECENT_QD2                   40913
 #define ID_OPEN_CART1                   40921
 #define ID_CLOSE_CART1                  40922
-#define ID_RECENT_CART1                 40923 // 40923-40930
-
+#define ID_RECENT_CART1                 40923
 #define ID_OPEN_CART2                   40931
 #define ID_CLOSE_CART2                  40932
-#define ID_RECENT_CART2                 40933 // 40933-40940
-
+#define ID_RECENT_CART2                 40933
 #define ID_PLAY_TAPE                    40941
 #define ID_REC_TAPE                     40942
 #define ID_CLOSE_TAPE                   40943
@@ -169,20 +148,16 @@
 #define ID_DIRECT_LOAD_MZT              40953
 #define ID_TAPE_BAUD_LOW                40954
 #define ID_TAPE_BAUD_HIGH               40955
-#define ID_RECENT_TAPE                  40961 // 40961-40968
-
+#define ID_RECENT_TAPE                  40961
 #define ID_OPEN_LASER_DISC              40971
 #define ID_CLOSE_LASER_DISC             40972
-#define ID_RECENT_LASER_DISC            40973 // 40973-40980
-
+#define ID_RECENT_LASER_DISC            40973
 #define ID_LOAD_BINARY1                 40981
 #define ID_SAVE_BINARY1                 40982
-#define ID_RECENT_BINARY1               40983 // 40983-40990
-
+#define ID_RECENT_BINARY1               40983
 #define ID_LOAD_BINARY2                 40991
 #define ID_SAVE_BINARY2                 40992
-#define ID_RECENT_BINARY2               40993 // 40993-41000
-
+#define ID_RECENT_BINARY2               40993
 #define ID_SCREEN_REC60                 41001
 #define ID_SCREEN_REC30                 41002
 #define ID_SCREEN_REC15                 41003
@@ -233,7 +208,6 @@
 #define ID_SCREEN_STRETCH_DOT           41055
 #define ID_SCREEN_STRETCH_ASPECT        41056
 #define ID_SCREEN_STRETCH_FILL          41057
-
 #define ID_SCREEN_MONITOR_TYPE0         41061
 #define ID_SCREEN_MONITOR_TYPE1         41062
 #define ID_SCREEN_MONITOR_TYPE2         41063
@@ -242,11 +216,9 @@
 #define ID_SCREEN_MONITOR_TYPE5         41066
 #define ID_SCREEN_MONITOR_TYPE6         41067
 #define ID_SCREEN_MONITOR_TYPE7         41068
-
 #define ID_SCREEN_CRT_FILTER            41071
 #define ID_SCREEN_SCANLINE              41072
 #define ID_SCREEN_ROTATE                41073
-
 #define ID_SOUND_REC                    41101
 #define ID_SOUND_STOP                   41102
 #define ID_SOUND_FREQ0                  41111
@@ -262,7 +234,6 @@
 #define ID_SOUND_LATE2                  41123
 #define ID_SOUND_LATE3                  41124
 #define ID_SOUND_LATE4                  41125
-
 #define ID_SOUND_DEVICE_TYPE0           41131
 #define ID_SOUND_DEVICE_TYPE1           41132
 #define ID_SOUND_DEVICE_TYPE2           41133
@@ -271,7 +242,6 @@
 #define ID_SOUND_DEVICE_TYPE5           41136
 #define ID_SOUND_DEVICE_TYPE6           41137
 #define ID_SOUND_DEVICE_TYPE7           41138
-
 #define ID_CAPTURE_FILTER               41201
 #define ID_CAPTURE_PIN                  41202
 #define ID_CAPTURE_SOURCE               41203
@@ -289,10 +259,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_RESOURCE_VALUE        110
 #define _APS_NEXT_COMMAND_VALUE         41213
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
-
