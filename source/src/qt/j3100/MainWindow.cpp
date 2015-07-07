a5146ce72bd2eea2dd9e9947428972ed93c40e86
K.Ohta <whatisthis.sowhat@gmail.com>
2015-07-07 17:29:28 +0900

    [Qt][VM][J3100] Add J3100GT.

---
+++ source/src/qt/j3100/MainWindow.cpp
@@ -0,0 +1,63 @@
+/*
+ * Common Source code Project:
+ * Ui->Qt->MainWindow for X1TurboZ .
+ * (C) 2015 K.Ohta <whatisthis.sowhat _at_ gmail.com>
+ *   License : GPLv2
+ *   History :
+ * Jan 14, 2015 : Initial, many of constructors were moved to qt/gui/menu_main.cpp.
+ */
+
+#include <QVariant>
+#include <QtGui>
+#include "menuclasses.h"
+#include "emu.h"
+#include "qt_main.h"
+
+
+void META_MainWindow::setupUI_Emu(void)
+{
+   int i;
+   QString tmps;
+   
+}
+
+void META_MainWindow::retranslateUi(void)
+{
+  int i;
+   
+  retranslateControlMenu("",  false);
+  retranslateFloppyMenu(0, 1);
+  retranslateFloppyMenu(1, 2);
+  retranslateSoundMenu();
+  retranslateScreenMenu();
+   
+  this->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+  
+  
+  actionAbout->setText(QApplication::translate("MainWindow", "About...", 0));
+  
+
+  menuEmulator->setTitle(QApplication::translate("MainWindow", "Emulator", 0));
+  menuMachine->setTitle(QApplication::translate("MainWindow", "Machine", 0));
+  
+  menuHELP->setTitle(QApplication::translate("MainWindow", "HELP", 0));
+   // Set Labels
+} // retranslateUi
+
+
+
+META_MainWindow::META_MainWindow(QWidget *parent) : Ui_MainWindow(parent)
+{
+   setupUI_Emu();
+   retranslateUi();
+}
+
+
+META_MainWindow::~META_MainWindow()
+{
+}
+
+//QT_END_NAMESPACE
+
+
+
