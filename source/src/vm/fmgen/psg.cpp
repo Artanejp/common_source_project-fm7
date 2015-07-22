8731117a584a4158c64f39a8906591b35af02516
Kyuma Ohta <whatisthis.sowhat@gmail.com>
2015-07-22 23:23:37 +0900

    [VM][FMGEN][WIN32] Fix warning on psg.

--- source/src/vm/fmgen/psg.cpp
+++ source/src/vm/fmgen/psg.cpp
@@ -134,7 +134,7 @@ void PSG::MakeEnvelopTable()
 		2,2, 2,0, 2,1, 2,3, 1,1, 1,3, 1,2, 1,0,
 	};
 	static uint8 table2[4] = {  0,  0, 31, 31 };
-	static uint8 table3[4] = {  0,  1, -1,  0 };
+	static uint8 table3[4] = {  0,  1, 255,  0 };
 
 	uint* ptr = enveloptable[0];
 
