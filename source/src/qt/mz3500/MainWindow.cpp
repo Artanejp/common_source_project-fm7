9a6bf622be926801fb4721c8cdb82fb0fcbe5d54
K.Ohta <whatisthis.sowhat@gmail.com>
2015-09-28 22:23:35 +0900

    [VM][Qt] Add MZ-3500.

---
+++ source/src/qt/mz3500/MainWindow.cpp
@@ -0,0 +1,76 @@
+/*
+ * Common Source code Project:
+ * Ui->Qt->MainWindow for X1TurboZ .
+ * (C) 2015 K.Ohta <whatisthis.sowhat _at_ gmail.com>
+ *   License : GPLv2
+ *   History :
+ * Jan 14, 2015 : Initial, many of constructors were moved to qt/gui/menu_main.cpp.
+ */
+
+#include <QVariant>
+#include <QtGui>
+#include "commonclasses.h"
+#include "menuclasses.h"
+#include "emu.h"
+#include "qt_main.h"
+
+//QT_BEGIN_NAMESPACE
+
+	
+
+void META_MainWindow::setupUI_Emu(void)
+{
+   menuMachine->setVisible(false);
+}
+
+void META_MainWindow::retranslateUi(void)
+{
+  retranslateControlMenu("Reset",  false);
+  retranslateFloppyMenu(0, 1);
+  retranslateFloppyMenu(1, 2);
+  retranslateFloppyMenu(2, 3);
+  retranslateFloppyMenu(3, 4);
+
+  retranslateSoundMenu();
+  retranslateScreenMenu();
+   
+  this->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+  
+  actionCapture_Screen->setText(QApplication::translate("MainWindow", "Capture Screen", 0));
+  
+  actionAbout->setText(QApplication::translate("MainWindow", "About...", 0));
+  
+
+  //	actionStart_Record_Movie->setText(QApplication::translate("MainWindow", "Start Record Movie", 0));
+  //      actionStop_Record_Movie->setText(QApplication::translate("MainWindow", "Stop Record Movie", 0));
+
+   menuScreen->setTitle(QApplication::translate("MainWindow", "Screen", 0));
+   menuStretch_Mode->setTitle(QApplication::translate("MainWindow", "Stretch Mode", 0));
+	
+//        menuRecord->setTitle(QApplication::translate("MainWindow", "Record", 0));
+//        menuRecoad_as_movie->setTitle(QApplication::translate("MainWindow", "Recoad as movie", 0));
+	
+  menuEmulator->setTitle(QApplication::translate("MainWindow", "Emulator", 0));
+  
+  menuHELP->setTitle(QApplication::translate("MainWindow", "HELP", 0));
+	actionHelp_AboutQt->setText(QApplication::translate("MainWindow", "About Qt", 0));
+   // Set Labels
+} // retranslateUi
+
+
+
+META_MainWindow::META_MainWindow(QWidget *parent) : Ui_MainWindow(parent)
+{
+   setupUI_Emu();
+   retranslateUi();
+}
+
+
+META_MainWindow::~META_MainWindow()
+{
+}
+
+//QT_END_NAMESPACE
+
+
+
