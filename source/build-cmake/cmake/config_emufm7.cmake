049beb134f6726e663d167d680db5e0b46ff07fb
K.Ohta <whatisthis.sowhat@gmail.com>
2015-12-20 00:51:41 +0900

    [BUILD][CMAKE] Fix dependency of GNU LD.

--- source/build-cmake/cmake/config_emufm7.cmake
+++ source/build-cmake/cmake/config_emufm7.cmake
@@ -8,7 +8,7 @@ message("** Start of configure CommonSourceProject,FM-8/7/77/AV, Qt **")
 message("")
 
 set(LOCAL_LIBS 	   
-		   common_common
+		   common_emu
                    qt_fm7
 		   qt_gui
 		   vm_fm7
