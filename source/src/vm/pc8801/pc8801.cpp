229d81f2d6a1bff6cec76deaa0df01a8361fd2c8
K.Ohta <whatisthis.sowhat@gmail.com>
2015-12-21 22:47:20 +0900

    [VM][PC8801] TRY: Reduce host CPU usage.

--- source/src/vm/pc8801/pc8801.cpp
+++ source/src/vm/pc8801/pc8801.cpp
@@ -106,7 +106,7 @@ VM::VM(EMU* parent_emu) : emu(parent_emu)
 #endif
 	
 #ifdef SUPPORT_PC88_HIGH_CLOCK
-	pc88event->set_context_cpu(dummycpu, 7987248 / 4);
+	pc88event->set_context_cpu(dummycpu, 7987248 / 8);
 	pc88event->set_context_cpu(pc88cpu, (config.cpu_type != 0) ? 3993624 : 7987248);
 #else
 	pc88event->set_context_cpu(dummycpu, 3993624 / 4);
