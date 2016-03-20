8e2c364a4b2795039c9bca332c77beb90c87fb9a
K.Ohta <whatisthis.sowhat@gmail.com>
2016-03-20 22:37:52 +0900

    [UI][Qt][MZ3500] Enable Dipswitches.

--- source/src/qt/machines/mz3500/MainWindow.cpp
+++ source/src/qt/machines/mz3500/MainWindow.cpp
@@ -16,16 +16,68 @@
 
 //QT_BEGIN_NAMESPACE
 
+Action_Control_MZ3500::Action_Control_MZ3500(QObject *parent) : Action_Control(parent)
+{
+	mz_binds = new Object_Menu_Control_MZ3500(parent);
+}
+
+Action_Control_MZ3500::~Action_Control_MZ3500(){
+	delete mz_binds;
+}
+
+Object_Menu_Control_MZ3500::Object_Menu_Control_MZ3500(QObject *parent) : Object_Menu_Control(parent)
+{
+}
+
+Object_Menu_Control_MZ3500::~Object_Menu_Control_MZ3500(){
+}
+
+void Object_Menu_Control_MZ3500::set_dipsw(bool flag)
+{
+	emit sig_dipsw(getValue1(), flag);
+}
 	
 
 void META_MainWindow::setupUI_Emu(void)
 {
-	menuMachine->setVisible(false);
+	int i;
+	QString tmps;
+	menuMachine->setVisible(true);
+	menu_Emu_DipSw = new QMenu(menuMachine);
+	menu_Emu_DipSw->setObjectName(QString::fromUtf8("menu_DipSw"));
+	actionGroup_DipSw = new QActionGroup(this);
+	actionGroup_DipSw->setExclusive(false);
+
+	menuMachine->addAction(menu_Emu_DipSw->menuAction());
+	
+	for(i = 0; i < 3; i++) {
+      	action_Emu_DipSw[i] = new Action_Control_MZ3500(this);
+        action_Emu_DipSw[i]->setCheckable(true);
+        tmps.number(i + 1);
+        tmps = QString::fromUtf8("actionEmu_DipSw") + tmps;
+        action_Emu_DipSw[i]->setObjectName(tmps);
+		menu_Emu_DipSw->addAction(action_Emu_DipSw[i]);
+			
+		actionGroup_DipSw->addAction(action_Emu_DipSw[i]);
+		connect(action_Emu_DipSw[i], SIGNAL(toggled(bool)),
+				action_Emu_DipSw[i]->mz_binds, SLOT(set_dipsw(bool)));
+		connect(action_Emu_DipSw[i]->mz_binds, SIGNAL(sig_dipsw(int, bool)),
+				this, SLOT(set_dipsw(int, bool)));
+	
+	}
+	action_Emu_DipSw[0]->mz_binds->setValue1(3);
+	action_Emu_DipSw[1]->mz_binds->setValue1(7);
+	action_Emu_DipSw[2]->mz_binds->setValue1(8);
+	if(((1 << 3) & config.dipswitch) != 0) action_Emu_DipSw[0]->setChecked(true);
+	if(((1 << 7) & config.dipswitch) != 0) action_Emu_DipSw[1]->setChecked(true);
+	if(((1 << 8) & config.dipswitch) != 0) action_Emu_DipSw[2]->setChecked(true);
+	
 }
 
 void META_MainWindow::retranslateUi(void)
 {
-	retranslateControlMenu("Reset",  false);
+	int i;
+	retranslateControlMenu("Halt",  true);
 	retranslateFloppyMenu(0, 1);
 	retranslateFloppyMenu(1, 2);
 	retranslateFloppyMenu(2, 3);
@@ -38,7 +90,15 @@ void META_MainWindow::retranslateUi(void)
 	retranslateUI_Help();
    
 	this->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
-   // Set Labels
+	menu_Emu_DipSw->setTitle(QApplication::translate("MainWindow", "Dip Switches", 0));
+	action_Emu_DipSw[0]->setText(QApplication::translate("MainWindow", "SW4: Period for Decimal Point", 0));
+	action_Emu_DipSw[1]->setText(QApplication::translate("MainWindow", "FD1: Normally Capital Letter", 0));
+	action_Emu_DipSw[2]->setText(QApplication::translate("MainWindow", "P/M: 3500 CG for 200 Line CRT", 0));
+
+//	actionPrintDevice[1]->setText(QApplication::translate("MainWindow", "Write to file", 0));
+	actionPrintDevice[1]->setText(QApplication::translate("MainWindow", "MZ-1P17", 0));
+
+		// Set Labels
 } // retranslateUi
 
 
--- source/src/qt/machines/mz3500/menuclasses.h
+++ source/src/qt/machines/mz3500/menuclasses.h
@@ -3,23 +3,48 @@
 #define _CSP_QT_MENUCLASSES_H
 
 #include "mainwidget.h"
+#include "commonclasses.h"
+#include "mainwidget.h"
 // This extends class CSP_MainWindow as Ui_MainWindow.
 // You may use this as 
 QT_BEGIN_NAMESPACE
 
+//  wrote of MZ3500 Specific menu.
+class Object_Menu_Control_MZ3500: public Object_Menu_Control
+{
+	Q_OBJECT
+ public:
+	Object_Menu_Control_MZ3500(QObject *parent);
+	~Object_Menu_Control_MZ3500();
+signals:
+	int sig_dipsw(int, bool);
+public slots:
+	void set_dipsw(bool);
+};
 
+class Action_Control_MZ3500 : public Action_Control
+{
+	Q_OBJECT
+public:
+	Object_Menu_Control_MZ3500 *mz_binds;
+	Action_Control_MZ3500(QObject *parent);
+	~Action_Control_MZ3500();
+};
 
 class Ui_MainWindow;
-//  wrote of X1 Specific menu.
 class META_MainWindow : public Ui_MainWindow {
-  Q_OBJECT
- protected:
-  void setupUI_Emu(void);
-   void retranslateUi(void);
- public:
-  META_MainWindow(QWidget *parent = 0);
-  ~META_MainWindow();
- public slots:
+	Q_OBJECT
+protected:
+	void setupUI_Emu(void);
+	void retranslateUi(void);
+	QMenu *menu_Emu_DipSw;
+	QActionGroup *actionGroup_DipSw;
+	Action_Control_MZ3500 *action_Emu_DipSw[3];
+  
+public:
+	META_MainWindow(QWidget *parent = 0);
+	~META_MainWindow();
+public slots:
 };
 
 QT_END_NAMESPACE
