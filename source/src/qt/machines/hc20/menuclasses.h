0d22724938e139fe4562ac7d148488c1374b0a55
K.Ohta <whatisthis.sowhat@gmail.com>
2016-03-20 22:40:36 +0900

    .

--- source/src/qt/machines/hc20/menuclasses.h
+++ source/src/qt/machines/hc20/menuclasses.h
@@ -15,40 +15,39 @@ class Ui_MainWindow;
 //  wrote of HC20 Specific menu.
 class Object_Menu_Control_HC20: public Object_Menu_Control
 {
-   Q_OBJECT
- public:
-     Object_Menu_Control_HC20(QObject *parent);
-     ~Object_Menu_Control_HC20();
+	Q_OBJECT
+public:
+	Object_Menu_Control_HC20(QObject *parent);
+	~Object_Menu_Control_HC20();
 signals:
-   int sig_dipsw(int, bool);
- public slots:
-   void set_dipsw(bool);
+	int sig_dipsw(int, bool);
+public slots:
+	 void set_dipsw(bool);
 };
 
 class Action_Control_HC20 : public Action_Control
 {
-   Q_OBJECT
- public:
-   Object_Menu_Control_HC20 *hc20_binds;
-   Action_Control_HC20(QObject *parent);
-   ~Action_Control_HC20();
+	Q_OBJECT
+public:
+	Object_Menu_Control_HC20 *hc20_binds;
+	Action_Control_HC20(QObject *parent);
+	~Action_Control_HC20();
 };
 
 class META_MainWindow : public Ui_MainWindow {
-  Q_OBJECT
- protected:
-  void setupUI_Emu(void);
-  void retranslateUi(void);
+	Q_OBJECT
+protected:
+	void setupUI_Emu(void);
+	void retranslateUi(void);
   
-  QMenu *menu_Emu_DipSw;
-  QActionGroup *actionGroup_DipSw;
-  Action_Control_HC20 *action_Emu_DipSw[4];
+	QMenu *menu_Emu_DipSw;
+	QActionGroup *actionGroup_DipSw;
+	Action_Control_HC20 *action_Emu_DipSw[4];
   
- public:
-  META_MainWindow(QWidget *parent = 0);
-  ~META_MainWindow();
+public:
+	META_MainWindow(QWidget *parent = 0);
+	~META_MainWindow();
 public slots:
-
 };
 
 QT_END_NAMESPACE
