2a7852d6f65f643cb5d878950e4d59a4e9970998
K.Ohta <whatisthis.sowhat@gmail.com>
2016-04-08 16:47:56 +0900

    [VM][UI][Qt] Add NEC PC-2001.

---
+++ source/src/qt/machines/pc2001/MainWindow.cpp
@@ -0,0 +1,59 @@
+/*
+ * Common Source code Project:
+ * Ui->Qt->MainWindow for N5200 .
+ * (C) 2015 K.Ohta <whatisthis.sowhat _at_ gmail.com>
+ *   License : GPLv2
+ *   History :
+ * Jan 14, 2015 : Initial, many of constructors were moved to qt/gui/menu_main.cpp.
+ */
+
+#include <QVariant>
+#include <QtGui>
+#include "commonclasses.h"
+#include "menuclasses.h"
+#include "emu.h"
+#include "qt_main.h"
+
+//QT_BEGIN_NAMESPACE
+
+	
+
+void META_MainWindow::setupUI_Emu(void)
+{
+	menuMachine->setVisible(false);
+}
+
+void META_MainWindow::retranslateUi(void)
+{
+	retranslateControlMenu(" ",  false);
+	retranslateCMTMenu();
+
+	retranslateSoundMenu();
+	retranslateScreenMenu();
+//	retranslateMachineMenu();
+	retranslateEmulatorMenu();
+	retranslateUI_Help();
+   
+	this->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+  
+	actionReset->setText(QApplication::translate("MainWindow", "Reset", 0));
+	// Set Labels
+} // retranslateUi
+
+
+
+META_MainWindow::META_MainWindow(QWidget *parent) : Ui_MainWindow(parent)
+{
+	setupUI_Emu();
+	retranslateUi();
+}
+
+
+META_MainWindow::~META_MainWindow()
+{
+}
+
+//QT_END_NAMESPACE
+
+
+
