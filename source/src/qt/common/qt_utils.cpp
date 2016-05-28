fcc52b4357dc2484116a8e53795eb581a6f197e9
K.Ohta <whatisthis.sowhat@gmail.com>
2016-05-28 19:36:49 +0900

    [Qt][UI] Fix saved file name for record as movie.

--- source/src/qt/common/qt_utils.cpp
+++ source/src/qt/common/qt_utils.cpp
@@ -105,7 +105,7 @@ void Object_Menu_Control::do_save_as_movie(void)
 	int fps = 0;
 	QDateTime nowTime = QDateTime::currentDateTime();
 	QString tmps = nowTime.toString(QString::fromUtf8("yyyy-MM-dd_hh-mm-ss.zzz."));
-	QString path = QString::fromUtf8("Saved_Movie") + tmps + QString::fromUtf8("mp4");
+	QString path = QString::fromUtf8("Saved_Movie_") + tmps + QString::fromUtf8("mp4");
 	if(emu->get_osd() == NULL) return;
 	path = QString::fromLocal8Bit(emu->get_osd()->get_app_path()) + path;
 
