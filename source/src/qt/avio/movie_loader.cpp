5701e1dbc19221e261c947c15ade6455c476ca64
K.Ohta <whatisthis.sowhat@gmail.com>
2016-07-30 09:39:47 +0900

    [WIP][Qt][MOVIE_LOADER] Constructing MOVIE_LOADER class.

---
+++ source/src/qt/avio/movie_loader.cpp
@@ -0,0 +1,520 @@
+#include "osd.h"
+#include "movie_loader.h"
+
+
+MOVIE_LOADER(OSD *osd, config_t *cfg) : public QObject(NULL);
+{
+	p_osd = osd;
+	p_cfg = cfg;
+	
+	frame_count = 0;
+	frame_rate = 29.97;
+	now_opening = false;
+	use_hwaccel = false;
+	_filename.clear();
+
+	video_format.clear();
+	video_codec.clear();
+	audio_codec.clear();
+	hwaccel_method.clear();
+
+	now_pausing = false;
+	now_playing = false;
+	
+	src_width = 640;
+	src_height = 200;
+	dst_width = 640;
+	dst_height = 200;
+#if defined(USE_LIBAV)
+	fmt_ctx = NULL;
+	video_dec_ctx = NULL;
+	audio_dec_ctx = NULL;
+	sws_context = NULL;
+	swr_context = NULL;
+	video_stream = NULL;
+	audio_stream = NULL;
+	for(int i = 0; i < 4; i++) video_dst_data[i] = NULL;
+	video_dst_bufsize = 0;
+	video_stream_idx = -1;
+	audio_stream_idx = -1;
+	frame = NULL;
+	video_frame_count = 0;
+	audio_frame_count = 0;
+	refcount = 0;
+#endif
+
+}
+
+#if defined(USE_LIBAV)
+int MOVIE_LOADER::decode_packet(int *got_frame, int cached)
+{
+    int ret = 0;
+    int decoded = pkt.size;
+
+    *got_frame = 0;
+
+    if (pkt.stream_index == video_stream_idx) {
+        /* decode video frame */
+        ret = avcodec_decode_video2(video_dec_ctx, frame, got_frame, &pkt);
+        if (ret < 0) {
+            fprintf(stderr, "Error decoding video frame (%s)\n", av_err2str(ret));
+            return ret;
+        }
+
+        if (*got_frame) {
+            if (frame->width != src_width || frame->height != src_height ||
+                frame->format != pix_fmt) {
+                /* To handle this change, one could call av_image_alloc again and
+                 * decode the following frames into another rawvideo file. */
+                fprintf(stderr, "Error: Width, height and pixel format have to be "
+                        "constant in a rawvideo file, but the width, height or "
+                        "pixel format of the input video changed:\n"
+                        "old: width = %d, height = %d, format = %s\n"
+                        "new: width = %d, height = %d, format = %s\n",
+                        src_width, src_height, av_get_pix_fmt_name(pix_fmt),
+                        frame->width, frame->height,
+                        av_get_pix_fmt_name(frame->format));
+                return -1;
+            }
+			if((old_dst_width != dst_width) || (old_dst_height != dst_height)) { // You sould allocate on opening.
+				if(tmp_frame != NULL) {
+					av_frame_free(tmp_frame);
+					tmp_frame = (AVFrame *)alloc_picture(AV_PIX_FMT_BGRA, dst_width, dst_height);
+					if (tmp_frame == NULL) {
+						AGAR_DebugLog(AGAR_LOG_INFO, "MOVIE_LOADER: Could not re-allocate video frame\n");
+						return -1;
+					}
+				}
+			}
+			old_dst_width = dst_width;
+			old_dst_height = dst_height;
+			
+            printf("video_frame%s n:%d coded_n:%d pts:%s\n",
+                   cached ? "(cached)" : "",
+                   video_frame_count++, frame->coded_picture_number,
+                   av_ts2timestr(frame->pts, &video_dec_ctx->time_base));
+			
+            /* copy decoded frame to destination buffer:
+             * this is required since rawvideo expects non aligned data */
+            //av_image_copy(video_dst_data, video_dst_linesize,
+            //              (const uint8_t **)(frame->data), frame->linesize,
+            //            pix_fmt, src_width, src_height);
+			
+			if(sws_context == NULL) {
+				sws_context = sws_getContext(frame->width, frame->height,
+											 AV_PIX_FMT_BGRA,
+											 dst_width, dst_height,
+											 pix_fmt,
+											 SCALE_FLAGS, NULL, NULL, NULL);
+				if (sws_context == NULL) {
+					AGAR_DebugLog(AGAR_LOG_INFO,
+								  "MOVIE_LOADER: Could not initialize the conversion context\n");
+					return -1;
+				}
+				
+			}
+			if(tmp_frame != NULL) { // You must pull final frame (and need locking?) before emit sig_decode_frames_complete().
+				ret = av_frame_make_writable(tmp_frame);
+				sws_scale(sws_context,
+						  frame->data, frame->linesize,
+						  0, dst_height, tmp_frame->data, tmp_frame->linesize);
+			}
+			
+		} else if (pkt.stream_index == audio_stream_idx) {
+			/* decode audio frame */
+			ret = avcodec_decode_audio4(audio_dec_ctx, frame, got_frame, &pkt);
+			if (ret < 0) {
+				fprintf(stderr, "Error decoding audio frame (%s)\n", av_err2str(ret));
+				return ret;
+			}
+			/* Some audio decoders decode only part of the packet, and have to be
+			 * called again with the remainder of the packet data.
+			 * Sample: fate-suite/lossless-audio/luckynight-partial.shn
+			 * Also, some decoders might over-read the packet. */
+			decoded = FFMIN(ret, pkt.size);
+			
+			if (*got_frame) {
+				size_t unpadded_linesize = frame->nb_samples * av_get_bytes_per_sample(frame->format);
+				printf("audio_frame%s n:%d nb_samples:%d pts:%s\n",
+					   cached ? "(cached)" : "",
+					   audio_frame_count++, frame->nb_samples,
+					   av_ts2timestr(frame->pts, &audio_dec_ctx->time_base));
+				
+				/* Write the raw audio data samples of the first plane. This works
+				 * fine for packed formats (e.g. AV_SAMPLE_FMT_S16). However,
+				 * most audio decoders output planar audio, which uses a separate
+				 * plane of audio samples for each channel (e.g. AV_SAMPLE_FMT_S16P).
+				 * In other words, this code will write only the first audio channel
+				 * in these cases.
+				 * You should use libswresample or libavfilter to convert the frame
+				 * to packed data. */
+				sound_data_cueue_t *px = malloc(sizeof(sound_data_cueue_t));
+				if(px != NULL) {
+					px->data = malloc((long)unpadded_linesize);
+					memcpy(px->data, (uint8_t *)(frame->extended_data[0]), (long)unpadded_linesize); // post tmp-buffer.
+					px->unpadded_linesize = (long)unpadded_linesize;
+					
+					snd_write_lock.lockForWrite();
+					sound_data_queue.enqueue(px);
+					snd_write_lock.unlock();
+				}
+				//fwrite(frame->extended_data[0], 1, unpadded_linesize, audio_dst_file);
+        }
+    }
+
+    /* If we use frame reference counting, we own the data and need
+     * to de-reference it when we don't use it anymore */
+    if (*got_frame && refcount)
+        av_frame_unref(frame);
+
+    return decoded;
+}
+
+int MOVIE_LOADER::open_codec_context(int *stream_idx,
+									 AVFormatContext *fmt_ctx, enum AVMediaType type)
+{
+    int ret, stream_index;
+    AVStream *st;
+    AVCodecContext *dec_ctx = NULL;
+    AVCodec *dec = NULL;
+    AVDictionary *opts = NULL;
+
+    ret = av_find_best_stream(fmt_ctx, type, -1, -1, NULL, 0);
+    if (ret < 0) {
+        fprintf(stderr, "Could not find %s stream in input file '%s'\n",
+                av_get_media_type_string(type), src_filename);
+        return ret;
+    } else {
+        stream_index = ret;
+        st = fmt_ctx->streams[stream_index];
+
+        /* find decoder for the stream */
+        dec_ctx = st->codec;
+        dec = avcodec_find_decoder(dec_ctx->codec_id);
+        if (!dec) {
+            fprintf(stderr, "Failed to find %s codec\n",
+                    av_get_media_type_string(type));
+            return AVERROR(EINVAL);
+        }
+
+        /* Init the decoders, with or without reference counting */
+        av_dict_set(&opts, "refcounted_frames", refcount ? "1" : "0", 0);
+        if ((ret = avcodec_open2(dec_ctx, dec, &opts)) < 0) {
+            fprintf(stderr, "Failed to open %s codec\n",
+                    av_get_media_type_string(type));
+            return ret;
+        }
+        *stream_idx = stream_index;
+    }
+
+    return 0;
+}
+
+int MOVIE_LOADER::get_format_from_sample_fmt(const char **fmt,
+											 enum AVSampleFormat sample_fmt)
+{
+    int i;
+    struct sample_fmt_entry {
+        enum AVSampleFormat sample_fmt; const char *fmt_be, *fmt_le;
+    } sample_fmt_entries[] = {
+        { AV_SAMPLE_FMT_U8,  "u8",    "u8"    },
+        { AV_SAMPLE_FMT_S16, "s16be", "s16le" },
+        { AV_SAMPLE_FMT_S32, "s32be", "s32le" },
+        { AV_SAMPLE_FMT_FLT, "f32be", "f32le" },
+        { AV_SAMPLE_FMT_DBL, "f64be", "f64le" },
+    };
+    *fmt = NULL;
+
+    for (i = 0; i < FF_ARRAY_ELEMS(sample_fmt_entries); i++) {
+        struct sample_fmt_entry *entry = &sample_fmt_entries[i];
+        if (sample_fmt == entry->sample_fmt) {
+            *fmt = AV_NE(entry->fmt_be, entry->fmt_le);
+            return 0;
+        }
+    }
+
+    fprintf(stderr,
+            "sample format %s is not supported as output format\n",
+            av_get_sample_fmt_name(sample_fmt));
+    return -1;
+}
+
+#endif // USE_LIBAV
+
+bool MOVIE_LOADER::open(QString filename)
+{
+    int ret = 0, got_frame;
+	_filename = filename;
+	
+    /* register all formats and codecs */
+    av_register_all();
+
+    /* open input file, and allocate format context */
+    if (avformat_open_input(&fmt_ctx, _filename.toLocal8Bit().constData(), NULL, NULL) < 0) {
+        fprintf(stderr, "Could not open source file %s\n", src_filename);
+        return -1;
+    }
+
+    /* retrieve stream information */
+    if (avformat_find_stream_info(fmt_ctx, NULL) < 0) {
+        fprintf(stderr, "Could not find stream information\n");
+        exit(1);
+    }
+
+    if (open_codec_context(&video_stream_idx, fmt_ctx, AVMEDIA_TYPE_VIDEO) >= 0) {
+        video_stream = fmt_ctx->streams[video_stream_idx];
+        video_dec_ctx = video_stream->codec;
+
+        /* allocate image where the decoded image will be put */
+        src_width = video_dec_ctx->width;
+        src_height = video_dec_ctx->height;
+        pix_fmt = video_dec_ctx->pix_fmt;
+        ret = av_image_alloc(video_dst_data, video_dst_linesize,
+                             src_width, src_height, pix_fmt, 1);
+        if (ret < 0) {
+            fprintf(stderr, "Could not allocate raw video buffer\n");
+            goto end;
+        }
+        video_dst_bufsize = ret;
+    }
+
+    if (open_codec_context(&audio_stream_idx, fmt_ctx, AVMEDIA_TYPE_AUDIO) >= 0) {
+        audio_stream = fmt_ctx->streams[audio_stream_idx];
+        audio_dec_ctx = audio_stream->codec;
+    }
+
+    /* dump input information to stderr */
+    av_dump_format(fmt_ctx, 0, src_filename, 0);
+
+    if (!audio_stream && !video_stream) {
+        fprintf(stderr, "Could not find audio or video stream in the input, aborting\n");
+        ret = 1;
+        goto end;
+    }
+
+    frame = av_frame_alloc();
+    if (!frame) {
+        fprintf(stderr, "Could not allocate frame\n");
+        ret = AVERROR(ENOMEM);
+        goto end;
+    }
+
+    /* initialize packet, set data to NULL, let the demuxer fill it */
+    av_init_packet(&pkt);
+    pkt.data = NULL;
+    pkt.size = 0;
+
+    /* read frames from the file */
+    while (av_read_frame(fmt_ctx, &pkt) >= 0) {
+        AVPacket orig_pkt = pkt;
+        do {
+            ret = decode_packet(&got_frame, 0);
+            if (ret < 0)
+                break;
+            pkt.data += ret;
+            pkt.size -= ret;
+        } while (pkt.size > 0);
+        av_packet_unref(&orig_pkt);
+    }
+
+    /* flush cached frames */
+    pkt.data = NULL;
+    pkt.size = 0;
+
+	// ToDo : Initialize SWScaler and SWresampler.
+	return true;
+end:
+	this->close();
+    return false;
+}
+
+void MOVIE_LOADER::close(void)
+{
+    if(video_dec_ctx != NULL) avcodec_close(video_dec_ctx);
+    if(audio_dec_ctx != NULL) avcodec_close(audio_dec_ctx);
+    avformat_close_input(&fmt_ctx);
+	if(sws_context != NULL) {
+		sws_freeContext(sws_context);
+	}
+	swr_free(swr_context);
+	swr_context = NULL;
+	sws_context = NULL;
+	
+	av_frame_free(&tmp_frame);
+	video_dec_ctx = NULL;
+	audio_dec_ctx = NULL;
+	sws_context = NULL;
+	now_playing = false;
+	
+}
+
+double MOVIE_LOADER::get_movie_frame_rate(void)
+{
+	return frame_rate;
+}
+
+int MOVIE_LOADER::get_movie_sound_rate(void)
+{
+	return sound_rate;
+}
+
+uint64_t MOVIE_LOADER::get_current_frame(void)
+{
+	return frame_count;
+}
+
+bool MOVIE_LOADER::is_playing(void)
+{
+	return now_playing;
+}
+
+
+bool MOVIE_LOADER::is_pausing(void)
+{
+	return now_pausing;
+}
+
+
+void MOVIE_LOADER::do_play()
+{
+	now_playing = true;
+	// ToDo: Uding libAV.
+	now_pausing = false;
+}
+
+void MOVIE_LOADER::do_stop()
+{
+	now_playing = false;
+	now_pauseing = false;
+	// ToDo: Stop sequence, clear buffer? ,
+	// Still not closing.
+}
+
+void MOVIE_LOADER::do_pause()
+{
+	if(now_playing) {
+		// Pause button
+		if(!now_pausing) {
+			// ToDo: Real
+		}
+		now_pausing = true;
+	}
+}
+
+void MOVIE_LOADER::do_mute(bool left, bool right)
+{
+	// Real Mute
+}
+
+void MOVIE_LOADER::do_decode_frames(int frames)
+{
+	int got_frame;
+	bool end_of_frame = false;
+	if(frames <= 0) {
+		do_dequeue_audio();
+		emit sig_decode_frames_comlete();
+		if(frames < 0) emit sig_decoding_error(MOVIE_LOADER_ILL_FRAME_NUMBER);
+		return;
+	}
+    for(int i = 0; i < frames; i++) {
+        decode_packet(&got_frame, 1);
+		if(got_frame == 0) {
+			end_of_frame = true;
+			break;
+		}
+    }
+	if(end_of_frame) {
+		// if frames > 1 then clear screen ?
+		do_dequeue_audio();
+		emit sig_decode_frames_comlete();
+		emit sig_movie_end(true);
+		return;
+	}
+	uint32_t cacheline[8];
+	uint32_t *p;
+	uint32_t *q;
+	int xx;
+	for(int yy = 0; yy < dst_height; yy++) {
+		p = (uint32_t *)(&(tmp_frame->data[0][yy * tmp_frame->linesize[0]]));
+		q = (uint32_t *)(p_osd->get_vm_screen_buffer(yy));
+		if((p == NULL) || (q == NULL)) break;
+		for(xx = dst_width; xx > 7;) {
+			cacheline[0] = p[0];
+			cacheline[1] = p[1];
+			cacheline[2] = p[2];
+			cacheline[3] = p[3];
+			cacheline[4] = p[4];
+			cacheline[5] = p[5];
+			cacheline[6] = p[6];
+			cacheline[7] = p[7];
+
+			q[0] = cacheline[0];
+			q[1] = cacheline[1];
+			q[2] = cacheline[2];
+			q[3] = cacheline[3];
+			q[4] = cacheline[4];
+			q[5] = cacheline[5];
+			q[6] = cacheline[6];
+			q[7] = cacheline[7];
+
+			p += 8;
+			q += 8;
+			xx -= 8;
+			if(xx < 8) break;
+		}
+		for(; xx > 0; xx--) {
+			*q++ = *p++;
+		}
+	}
+	do_dequeue_audio();
+	emit sig_decode_frames_comlete();
+	return;
+}
+
+void MOVIE_SAVER::do_seek_frame(bool relative, int frames)
+{
+	// TODO.
+}
+
+void MOVIE_LOADER::do_dequeue_audio()
+{
+	long audio_data_size = 0;
+	sound_data_queue_t *tmpq = NULL;
+	
+	snd_write_lock.lockForWrite();
+	int il = sound_data_queue.count();
+	while(il > 0) {
+		tmpq = sound_data_queue.at(il - 1);
+		if(tmpq != NULL) {
+			audio_data_size += tmpq->unpadded_linesize;
+		}
+		il = il - 1;
+	}
+	tmpq = NULL;
+	uint8_t *tmpdata = NULL;
+	long dptr = 0;
+	if(audio_data_size > 0) {
+		tmpdata = malloc(audio_data_size);
+		int ii;
+		il = sound_data_queue.count();
+		for(ii = 0; ii < il; ii++) {
+			tmpq = sound_data_queue.dequeue();
+			if(tmpq != NULL) {
+				if(tmpq->data != NULL) {
+					if((tmpq->unpadded_linesize != 0) && (dptr < audio_data_size)) {
+						if(tmpdata != NULL) {
+							memcpy(&tmpdata[dptr], tmpq->data, tmpq->unpadded_linesize);
+						}
+						dptr += tmpq->unpadded_linesize;
+					}
+					free(tmpq->data);
+				}
+				free(tmpq);
+			}
+		}
+	}
+	snd_write_lock.unlock();
+	if(tmpdata != NULL) emit sig_send_audio_frame(tmpdata,dptr); // Please free arg1 after calling sound_callback of vm.
+}	
+
+	
---
+++ source/src/qt/avio/movie_loader.h
@@ -0,0 +1,132 @@
+/*
+ * Common Source Code Project for Qt : movie saver.
+ * (C) 2016 K.Ohta <whatisthis.sowhat _at_ gmail.com>
+ *  License: GPLv2
+ *  History: May 27, 2016 : Initial. This refer from ffmpeg: doc/examples/demuxing_decoding.c.
+ */
+
+#ifndef _QT_OSD_MOVIE_LOADER_H
+#define _QT_OSD_MOVIE_LOADER_H
+
+#if defined(USE_LIBAV)
+extern "C" {
+	#include "libavutil/channel_layout.h"
+	#include "libavutil/opt.h"
+	#include "libavutil/mathematics.h"
+	#include "libavutil/timestamp.h"
+	#include "libavutil/imgutils.h"
+	#include "libavutil/samplefmt.h"
+	#include "libavformat/avformat.h"
+	#include "libswscale/swscale.h"
+	#include "libswresample/swresample.h"
+}
+#endif
+
+
+// Copy from FFMPEG-3.0.2; doc/example/muxing.c .
+#define STREAM_PIX_FMT	AV_PIX_FMT_RGBA /* default pix_fmt */
+
+//#define SCALE_FLAGS SWS_BICUBLIN
+#define SCALE_FLAGS SWS_POINT
+
+#include <QObject>
+#include <QReadWriteLock>
+#include <QWaitCondition>
+
+#include "config.h"
+
+typedef struct {
+	uint8_t *data;
+	long unpadded_linesize;
+} sound_data_cueue_t;
+
+QT_BEGIN_NAMESPACE
+class OSD;
+class MOVIE_LOADER: public QObject
+{
+	Q_OBJECT
+private:
+#if defined(USE_LIBAV)
+	AVFormatContext *fmt_ctx; // = NULL;
+	AVCodecContext *video_dec_ctx;// = NULL
+	AVCodecContext *audio_dec_ctx;
+	enum AVPixelFormat pix_fmt;
+	AVStream *video_stream, *audio_stream; // NULL
+	uint8_t *video_dst_data[4]; // = {NULL};
+	int      video_dst_linesize[4];
+	int video_dst_bufsize;
+	int video_stream_idx, audio_stream_idx; //int video_stream_idx = -1, audio_stream_idx = -1;
+	AVFrame *frame; //AVFrame *frame = NULL;
+	AVPacket pkt;
+	int video_frame_count; // = 0;
+	int audio_frame_count; // = 0;
+	int refcount; // = 0
+	int decode_packet(int *got_frame, int cached);
+	int open_codec_context(int *stream_idx, AVFormatContext *fmt_ctx, enum AVMediaType type);
+	int get_format_from_sample_fmt(const char **fmt, enum AVSampleFormat sample_fmt);
+
+#endif
+protected:
+	OSD *p_osd;
+	config_t *p_cfg;
+	uint64_t frame_count;
+	double frame_rate;
+	int sound_rate;
+	bool now_opening;
+	QString _filename;
+	
+	bool use_hwaccel;
+	QString video_format;
+	QString video_codec;
+	QString audio_codec;
+	QString hwaccel_method;
+
+	bool now_pausing;
+	bool now_playing;
+	
+	int src_width, src_height;
+	int dst_width, dst_height;
+
+	QList<sound_data_cueue_t *> sound_data_queue;
+public:
+	MOVIE_LOADER(OSD *osd, config_t *cfg);
+	~MOVIE_LOADER();
+	bool open(QString filename);
+	bool close();
+	double  get_movie_frame_rate(void);
+	int get_movie_sound_rate(void);
+	
+	QReadWriteLock frame_lock;
+	QWaitCondition lock_cond;
+	
+	uint64_t get_current_frame(void);
+	bool is_playing(void);
+	bool is_pausing(void);
+
+public slots:
+	void do_set_dst_geometry(int width, int height);
+	void do_set_enable_hwaccel_decoding(bool enable);
+	void do_set_enable_hwaccel_scaling(bool enable);
+	void do_set_dst_pixfmt(int type);
+	
+	void do_play();
+	void do_stop();
+	void do_pause();
+//	void do_fast_forward(int ticks);
+//	void do_fast_rewind(int ticks);
+	void do_mute(bool left, bool right);
+
+	void do_decode_frames(int frames);
+	void do_seek_frame(bool relative, int frames);
+	void do_dequeue_audio();
+	
+signals:
+	int sig_call_sound_callback(uint8_t *, long); // Call callback.
+	int sig_decode_frames_complete(void); // Decode frames are completed.
+	int sig_movie_end(bool); // MOVIE END
+	int sig_movie_ready(bool); // ACK
+	int sig_decoding_error(int); // error_num
+};
+QT_END_NAMESPACE
+#endif //_QT_OSD_MOVIE_LOADER_H
+
