937dd99e5f1a2bc68f0b6062431a94abf4bd9786
K.Ohta <whatisthis.sowhat@gmail.com>
2017-12-04 13:51:59 +0900

    [INSTALLER] Update.

--- source/tool/installer_unix.sh
+++ source/tool/installer_unix.sh
@@ -7,7 +7,7 @@ LDCONFIG=/sbin/ldconfig
 CSP_ARCH="x86_64-linux-gnu"
 MULTIARCH="Yes"
 CSP_PREFIX=/usr/local
-CSP_GUILIB="libCSPgui.so.2.10.4 libCSPosd.so.2.9.11 libCSPemu_utils.so.2.6.1 libCSPavio.2.6.10 libCSPfmgen.so.1.1.0 libCSPcommon_vm.so.1.1.20"
+CSP_GUILIB="libCSPgui.so.2.10.5 libCSPosd.so.2.9.12 libCSPemu_utils.so.2.6.2 libCSPavio.2.6.12 libCSPfmgen.so.1.1.1 libCSPcommon_vm.so.1.1.21"
 
 for i in "$@"; do
   case "$1" in 
