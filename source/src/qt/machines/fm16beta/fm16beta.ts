7aaaad6455fc66b9aed368bff2e500c98d622195
K.Ohta <whatisthis.sowhat@gmail.com>
2018-03-01 15:23:20 +0900

    [UI][Qt][FM16BETA] Initial support.

---
+++ source/src/qt/machines/fm16beta/fm16beta.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="MainWindow.cpp" line="42"/>
+        <source>MainWindow</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
