604bafa199f22330e98da3db6d1ef87110eac4ae
K.Ohta <whatisthis.sowhat@gmail.com>
2019-05-18 23:19:05 +0900

    [Build][WIN32] Update cross-build scripts for Qt.

--- source/tool/cross-build/Qt5.12/config_sample.5.12.sh
+++ source/tool/cross-build/Qt5.12/config_sample.5.12.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
-export PATH="$PATH:\
-            /usr/local/i586-mingw-msvc/icu/bin:\
-	    /usr/local/i586-mingw-msvc/Angle/bin:\
-	    /usr/local/i586-mingw-msvc/SDL/i686-w64-mingw32/bin: \
-            /usr/local/i586-mingw-msvc/DirectX_June_2010/Developer\ Runtime/x86 \
-            "
-export PKG_CONFIG_LIBDIR=/usr/i686-w64-mingw32/lib
-export PKG_CONFIG_SYSROOT_DIR=/usr/i686-w64-mingw32
+VULKAN_SDK="/usr/local/i586-mingw-msvc/Vulkan"
+SDK_PREFIX="/usr/local/i586-mingw-msvc"
+#export PATH="$PATH:~/src/fxc2"
+export PATH="$PATH:$SDK_PREFIX/icu/bin"
+export PATH="$PATH:$SDK_PREFIX/Angle/bin"
+export PATH="$PATH:$SDK_PREFIX/SDL/i686-w64-mingw32/bin"
+export PATH="$PATH:$SDK_PREFIX/DirectX_June_2010/Utilities/bin/x86"
+export PATH="$PATH:$SDK_PREFIX/DirectX_June_2010/Developer Runtime/x86"
+export PATH="$PATH:$VULKAN_SDK/bin"
 
+
+export PKG_CONFIG_LIBDIR=/usr/local/i586-mingw-msvc/pkgconfig/lib
+export PKG_CONFIG_PATH=/usr/local/i586-mingw-msvc/pkgconfig/lib/pkgconfig
+export PKG_CONFIG_SYSROOT_DIR=/usr/local/i586-mingw-msvc/pkgconfig
+#export QMAKE_DXSDK_DIR=/usr/local/i586-mingw-msvc/DirectX_June_2010/
 
 #wine ./qtbase/configure.exe \
 ./configure \
@@ -29,19 +35,21 @@ export PKG_CONFIG_SYSROOT_DIR=/usr/i686-w64-mingw32
 	    -skip qtquickcontrols \
 	    -skip qtlocation \
 	    -icu \
-	    -nomake examples \
 	    -nomake tests \
-	    -I /usr/local/i586-mingw-msvc/SDL/i686-w64-mingw32/include/SDL2 \
-	    -L /usr/local/i586-mingw-msvc/SDL/i686-w64-mingw32/lib \
-	    -I /usr/local/i586-mingw-msvc/DirectX_June_2010/Include \
-	    -L /usr/local/i586-mingw-msvc/DirectX_June_2010/Lib/x86 \
-	    -I /usr/local/i586-mingw-msvc/Angle/include \
-	    -L /usr/local/i586-mingw-msvc/Angle/lib \
-	    -L /usr/local/i586-mingw-msvc/Angle/bin \
-	    -device-option SDL_PREFIX=/usr/local/i586-mingw-msvc/SDL/i686-w64-mingw32 \
-	    -device-option SDL2_PREFIX=/usr/local/i586-mingw-msvc/SDL/i686-w64-mingw32 \
-	    -device-option ICU_PREFIX=/usr/local/i586-mingw-msvc/icu \
-	    -device-option OPENGL_ES2_PREFIX=/usr/local/i586-mingw-msvc/Angle \
+	    -c++std c++14 \
+	    -I $SDK_PREFIX/SDL/i686-w64-mingw32/include/SDL2 \
+	    -I $SDK_PREFIX/SDL/i686-w64-mingw32/include \
+	    -L $SDK_PREFIX/SDL/i686-w64-mingw32/lib \
+	    -I $SDK_PREFIX/DirectX_June_2010/Include \
+	    -L $SDK_PREFIX/DirectX_June_2010/Lib/x86 \
+	    -I $VULKAN_SDK/include \
+	    -I $VULKAN_SDK/include/vulkan \
+	    -L $VULKAN_SDK/lib \
+	    -device-option SDL_PREFIX=$SDK_PREFIX/SDL/i686-w64-mingw32 \
+	    -device-option SDL2_PREFIX=$SDK_PREFIX/SDL/i686-w64-mingw32 \
+	    -device-option ICU_PREFIX=$SDK_PREFIX/icu \
+	    -device-option OPENGL_ES2_PREFIX=$SDK_PREFIX/Angle \
+	    -device-option VULKAN_PREFIX=$VULKAN_SDK \
 	    -device-option LIBS_OPENGL_ES2+=GLESv2 \
 	    -device-option LIBS_OPENGL_ES2+=EGL \
 	    -device-option LIBEGL_NAME=EGL.dll \
@@ -49,9 +57,23 @@ export PKG_CONFIG_SYSROOT_DIR=/usr/i686-w64-mingw32
 	    -opengl dynamic \
 	    -no-eglfs \
 	    -no-evr \
+	    -feature-direct3d9 \
+	    -feature-vulkan \
             -pkg-config \
 	    $@ \
 
+#	    -device-option QMAKE_DXSDK_DIR=/usr/local/i586-mingw-msvc/DirectX_June_2010 \
+#	    -device-option DXSDK_DIR=/usr/local/i586-mingw-msvc/DirectX_June_2010 \
+#	    -angle \
+#	    -feature-dxguid \
+#	    -feature-direct3d9 \
+#	    -feature-vulkan \
+#
+#	    -nomake examples \
+
+#	    -I /usr/local/i586-mingw-msvc/Angle/include \
+#	    -L /usr/local/i586-mingw-msvc/Angle/lib \
+#	    -L /usr/local/i586-mingw-msvc/Angle/bin \
 #	    -D GL_GLEXT_PROTOTYPES \
 #	    -opengl es2 \
 #	    -opengles3 \
