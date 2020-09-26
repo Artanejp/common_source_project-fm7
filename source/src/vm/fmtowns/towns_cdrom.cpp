cdfc81b7398812d0f9206d5b978808fcbc5ebf0e
K.Ohta <whatisthis.sowhat@gmail.com>
2020-09-27 00:32:11 +0900

    [VM][FMTOWNS][CDROM] Re-Disable value handling around command 80h.

--- source/src/vm/fmtowns/towns_cdrom.cpp
+++ source/src/vm/fmtowns/towns_cdrom.cpp
@@ -568,6 +568,7 @@ void TOWNS_CDROM::execute_command(uint8_t command)
 				status_media_changed(false);
 				break;;
 			}
+#if 0			
 			if(toc_table[current_track].is_audio) {
 //				if((param_queue[0] == 0x08)) {
 					switch(prev_command & 0x9f)
@@ -634,9 +635,10 @@ void TOWNS_CDROM::execute_command(uint8_t command)
 				} else {
 					status_accept(0, 0x00, 0x00);
 				}
-				}
+			}
+#endif
 //			stat_reply_intr = true;
-//			status_accept(0, 0x00, 0x00);
+			status_accept(0, 0x00, 0x00);
 //			if(stat_reply_intr) set_mcu_intr(true);
 		}
 		break;
