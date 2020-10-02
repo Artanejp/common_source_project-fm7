3016ef4cb5b05ba43f2f880270ef038317ee80f4
K.Ohta <whatisthis.sowhat@gmail.com>
2020-10-02 19:17:57 +0900

    [BUILD_DEFAULT] Fix TOOLCHAIN's path.

--- source/build-cmake/cmake/build_default_vars.sh
+++ source/build-cmake/cmake/build_default_vars.sh
@@ -1,5 +1,5 @@
-cmake .. -DCMAKE_TOOLCHAINFILE=../build-cmake/cmake/toolchain_native_llvm.cmake \
+cmake .. -DCMAKE_TOOLCHAIN_FILE="$PWD/../build-cmake/cmake/toolchain_native_llvm.cmake" \
       -DCMAKE_BUILD_TYPE=Relwithdebinfo \
-      -DCMAKE_C_FLAGS="-g -gz -flto -O2" \
-      -DCMAKE_CXX_FLAGS="-g -gz -flto -O2" \
-      -DCMAKE_EXE_LINKER_FLAGS="-g -gz -flto -O2"
+      -DCMAKE_C_FLAGS_RELWITHDEBINFO="-g2 -flto -O2" \
+      -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-g2 -flto -O2" \
+      -DCMAKE_EXE_LINKER_FLAGS="-g2 -gz -flto -O2"
