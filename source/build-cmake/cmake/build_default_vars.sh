27092b2bb9aa8c92ee6e6df10fcd62de07c96f6c
K.Ohta <whatisthis.sowhat@gmail.com>
2020-10-02 22:11:34 +0900

    [BUILD][CMAKE] Fix FTBFS with CLANG.

--- source/build-cmake/cmake/build_default_vars.sh
+++ source/build-cmake/cmake/build_default_vars.sh
@@ -1,5 +1,33 @@
 cmake .. -DCMAKE_TOOLCHAIN_FILE="$PWD/../build-cmake/cmake/toolchain_native_llvm.cmake" \
       -DCMAKE_BUILD_TYPE=Relwithdebinfo \
-      -DCMAKE_C_FLAGS_RELWITHDEBINFO="-g2 -flto -O2" \
-      -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-g2 -flto -O2" \
-      -DCMAKE_EXE_LINKER_FLAGS="-g2 -gz -flto -O2"
+      -DCMAKE_C_FLAGS_RELWITHDEBINFO=" \
+      		-g2 \
+		-ggdb \
+		-gz=zlib \
+		-O3 \
+		-msse2 \
+		-mfpmath=sse \
+ 	        -Wreserved-user-defined-literal \
+ 	        -fslp-vectorize \
+ 	        -fvectorize \
+		-fstrict-vtable-pointers \
+		-fstrict-enums \
+		" \
+      -DCMAKE_CXX_FLAGS_RELWITHDEBINFO=" \
+      		-g2 \
+		-ggdb \
+		-gz=zlib \
+		-O3 \
+		-msse2 \
+		-mfpmath=sse \
+ 	        -Wreserved-user-defined-literal \
+ 	        -fslp-vectorize \
+ 	        -fvectorize \
+		-fstrict-vtable-pointers \
+		-fstrict-enums \
+		" \
+      -DCMAKE_EXE_LINKER_FLAGS="\
+      		-g2 \
+		-ggdb \
+		-gz=zlib \
+		" \
