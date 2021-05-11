14cd25acd0c2e57f7054f50aad3f7b7344c62562
K.Ohta <whatisthis.sowhat@gmail.com>
2021-05-11 13:29:26 +0900

    [BUILD][CMake] Add samplescripts supports GCC-11 initially.

---
+++ source/sample-scripts/build_default_vars.gcc11.sh
@@ -0,0 +1,41 @@
+cmake .. -DCMAKE_TOOLCHAIN_FILE="$PWD/../cmake/toolchains/toolchain_native_gcc11.cmake" \
+      -DCMAKE_BUILD_TYPE=Relwithdebinfo \
+      -DCMAKE_C_FLAGS_RELWITHDEBINFO=" \
+      		-gz \
+      		-ggdb \
+		-O3 \
+		-msse2 \
+		-mfpmath=sse \
+		-flto \
+		-flto-compression-level=9 \
+		-ffat-lto-objects \
+		-Wa,--compress-debug-sections=zlib \
+		" \
+      -DCMAKE_CXX_FLAGS_RELWITHDEBINFO=" \
+      		-gz \
+      		-ggdb \
+		-O3 \
+		-msse2 \
+		-flto \
+		-flto-compression-level=9 \
+		-ffat-lto-objects \
+		-Wa,--compress-debug-sections=zlib \
+		" \
+      -DCMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO="\
+		-ggdb \
+		-gz \
+		-O3 \
+		-flto=6 \
+		-msse2 \
+		-Wl,--compress-debug-sections=zlib \
+		-Wa,--compress-debug-sections=zlib \
+		" \
+      -DCMAKE_MODULE_LINKER_FLAGS_RELWITHDEBINFO="\
+      		-ggdb \
+		-gz \
+		-O3 \
+		-flto=6 \
+		-msse2 \
+		-Wl,--compress-debug-sections=zlib \
+		-Wa,--compress-debug-sections=zlib \
+		" \
