c024343624e6e1b22710397952f9976c6787d180
K.Ohta <whatisthis.sowhat@gmail.com>
2021-12-29 22:35:12 +0900

    [VM][MCS48] Fix larger default value for UINT32_MAX for default value.

--- source/src/vm/mcs48.h
+++ source/src/vm/mcs48.h
@@ -507,7 +507,7 @@ public:
 	{
 		d_io = device;
 	}
-	void set_context_intr(DEVICE* device, uint32_t bit = 0xfffffffff)
+	void set_context_intr(DEVICE* device, uint32_t bit = 0xffffffff)
 	{
 		d_intr = device;
 	}
