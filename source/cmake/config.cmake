a120844fdda561f6abc3dabb820a63bb2b38a148
K.Ohta <whatisthis.sowhat@gmail.com>
2022-01-17 01:10:38 +0900

    [Qt][Build][CMake] Prepare to support Qt6 (maybe later than Qt6.2).

--- source/cmake/config.cmake
+++ source/cmake/config.cmake
@@ -138,9 +138,14 @@ add_definitions(-DUSE_QT)
 # 20210914 K.O Start to migrate for Qt6.
 # See, https://doc.qt.io/qt-6/portingguide.html
 if(${Qt5Widgets_VERSION_MAJOR} VERSION_GREATER "4")
+  if(${Qt5Widgets_VERSION_MAJOR} VERSION_EQUAL "5")
     if(${Qt5Widgets_VERSION_MINOR} VERSION_GREATER "14")
-        add_compile_definitions(QT_DISABLE_DEPRECATED_BEFORE=0x050F00)
+      add_compile_definitions(QT_DISABLE_DEPRECATED_BEFORE=0x050F00)
     endif()
+  else()
+	#Qt6 or later
+    add_compile_definitions(QT_DISABLE_DEPRECATED_BEFORE=0x060000)
+  endif()
 endif()
 
 add_definitions(-DQT_MAJOR_VERSION=${Qt5Widgets_VERSION_MAJOR})
