23e4a23888b39b7e566d234c54cfda320a2bffe7
K.Ohta <whatisthis.sowhat@gmail.com>
2022-01-30 16:12:34 +0900

    [VM][EVENT] Enlarge management devices, sounds and events.This may need future 32bit/16bit emulator.
    [VM][EVENT][DOC] Add description of some methods for DOXYGEN.

--- source/src/vm/event.h
+++ source/src/vm/event.h
@@ -17,44 +17,55 @@
 
 #include <typeinfo>
 
-#define MAX_DEVICE	64
-#define MAX_CPU		8
-#define MAX_SOUND	32
-#define MAX_LINES	1024
-#define MAX_EVENT	64
-#define NO_EVENT	-1
+#define MAX_DEVICE	128		/*!< Maximum devices to manage. */
+#define MAX_CPU		8		/*!> Maximum number of CPUs to RUN. */
+#define MAX_SOUND	32		/*!< Maximum number of sound sources. */
+#define MAX_LINES	1024	/*!< Maximum lines per a frame. */
+#define MAX_EVENT	128		/*!< Maximum events to manage. */
+#define NO_EVENT	-1		/*!< This has no event */
 #if !defined(MAX_SOUND_IN_BUFFERS)
-	#define MAX_SOUND_IN_BUFFERS 8
+	#define MAX_SOUND_IN_BUFFERS 8	/*! Maximum sound input sources. */
 #else
 	#if (MAX_SOUND_IN_BUFFERS <= 0)
 	#define MAX_SOUND_IN_BUFFERS 8
 	#endif
 #endif
 
-
+/*! 
+  @class EVENT
+  @brief EVENT manager, includes CPUs execution.
+  @note Executing event has run per half of a frame by default at CSP/Qt.
+*/
 class EVENT : public DEVICE
 {
 private:
 	// event manager
+	/*!
+	  @brief Structure for executing CPUs
+	  @see EVENT::drive()
+	*/
 	typedef struct {
-		DEVICE* device;
-		uint32_t cpu_clocks;
-		uint32_t update_clocks;
-		uint32_t accum_clocks;
+		DEVICE* device;			//!< Target Device ID
+		uint32_t cpu_clocks;	//!< Target CLOCK by 1Hz. 
+		uint32_t update_clocks;	//!< Target clock for updating scheduler. 
+		uint32_t accum_clocks;	//!< Target clock for accumulation.
 	} cpu_t;
-	cpu_t d_cpu[MAX_CPU];
+	cpu_t d_cpu[MAX_CPU];		//!< TARGET CPU entries to run.
 
 	uint32_t cpu_update_clocks[MAX_CPU][6];
-	int dcount_cpu;
+	int dcount_cpu;	//! Numbers of Target CPUs.
 
-	bool event_half;
+	bool event_half;	//! Display second half of frame. 
 	int frame_clocks;
 	int vline_clocks[MAX_LINES];
 	int power;
 	int event_remain, event_extra;
 	int cpu_remain, cpu_accum, cpu_done;
 	uint64_t event_clocks;
-	
+
+	/*!
+	  @brief event management structure.
+	*/
 	typedef struct event_t {
 		DEVICE* device;
 		int event_id;
@@ -170,19 +181,36 @@ public:
 	void update_config();
 	bool process_state(FILEIO* state_fio, bool loading);
 	
-	// common event functions
+	//! common event functions
+	/*! 
+	  @brief Get DEVICE ID of event manager.
+	*/
 	int get_event_manager_id()
 	{
 		return this_device_id;
 	}
+	/*! 
+	  @brief Get clock Hz of first CPU.
+	*/
 	uint32_t get_event_clocks()
 	{
 		return d_cpu[0].cpu_clocks;
 	}
+	/*!
+	  @brief Determine whether device is Primary CPU.
+	  @param device Device pointer to check.
+	  @return true if primary CPU (CPU #0).
+	*/
 	bool is_primary_cpu(DEVICE* device)
 	{
 		return (d_cpu[0].device == device);
 	}
+	/*!
+	  @brief Get clock of target CPU device.
+	  @param device target device pointer.
+	  @return clock Hz of target.
+	  @note return default clock value (= CPU #0) if device is not as CPU.
+	*/
 	uint32_t __FASTCALL get_cpu_clocks(DEVICE* device)
 	{
 		for(int index = 0; index < dcount_cpu; index++) {
@@ -192,20 +220,39 @@ public:
 		}
 		return CPU_CLOCKS;
 	}
+	/*!
+	 @brief Set new framerate of this VM.
+	 @param new_frames_per_sec Framerate after next frame rate by 1Sec.
+	 @note This change will effect after frame rate, not at this frame rate.
+	*/
 	void set_frames_per_sec(double new_frames_per_sec)
 	{
 		next_frames_per_sec = new_frames_per_sec;
 	}
+	/*!
+	 @brief Set new lines per frame of this VM.
+	 @param new_lines_per_frame Lines after next frame period.
+	 @note This change will effect after frame period, not at this frame period.
+	 @note Lines limits from 1 to MAX_LINES .
+	*/
 	void set_lines_per_frame(int new_lines_per_frame)
 	{
 		if(new_lines_per_frame < MAX_LINES) {
 			next_lines_per_frame = new_lines_per_frame;
 		}
 	}
+	/*!
+	  @brief Get lines per frame of next frame period.
+	  @return Lines per frame of next frame period, not current period.
+	*/
 	int get_lines_per_frame()
 	{
 		return next_lines_per_frame;
 	}
+	/*!
+	  @brief Update extra events if remains host time.
+	  @param clock clocks. Still dummy.
+	*/ 
 	void __FASTCALL update_extra_event(int clock);
 	void register_event(DEVICE* device, int event_id, double usec, bool loop, int* register_id);
 	void register_event_by_clock(DEVICE* device, int event_id, uint64_t clock, bool loop, int* register_id);
@@ -219,10 +266,18 @@ public:
 	double __FASTCALL get_passed_usec(uint32_t prev);
 	uint32_t get_passed_clock_since_vline();
 	double get_passed_usec_since_vline();
+	/*!
+	 @brief Get current proccessing position of vertical line.
+	 @return Cureent position.
+	*/
 	int get_cur_vline()
 	{
 		return cur_vline;
 	}
+	/*!
+	 @brief Get relative clock position value at current line.
+	 @return Cureent clock position.
+	*/
 	int get_cur_vline_clocks()
 	{
 		return vline_clocks[cur_vline];
@@ -235,6 +290,11 @@ public:
 	double get_current_usec();
 	uint32_t __FASTCALL get_cpu_clock(int index);
 	// unique functions
+	/*!
+	  @brief Get frame rate of next frame period.
+	  @return Frame rate by Hz.
+	  @note This doesn't return FPS of this period.
+	*/
 	double get_frame_rate()
 	{
 		return next_frames_per_sec;
@@ -261,10 +321,17 @@ public:
 	int __FASTCALL get_sound_in_latest_data(int bank, int32_t* dst, int expect_channels);
 	int __FASTCALL get_sound_in_data(int bank, int32_t* dst, int expect_samples, int expect_rate, int expect_channels);
 	int rechannel_sound_in_data(int32_t*dst, int16_t* src, int dst_channels, int src_channels, int samples);
-	
+
+	/*!
+	  @brief Register CPU to event manager.
+	  @param device Pointer of CPU DEVICE to register.
+	  @param clocks Basic clock Hz of this device.
+	  @return index number of regitered. -1 if failed to register.
+	*/
 	int set_context_cpu(DEVICE* device, uint32_t clocks = CPU_CLOCKS)
 	{
 		assert(dcount_cpu < MAX_CPU);
+		if(dcount_cpu >= MAX_CPU) return -1;
 		int index = dcount_cpu++;
 		d_cpu[index].device = (DEVICE *)device;
 		d_cpu[index].cpu_clocks = clocks;
@@ -272,6 +339,14 @@ public:
 		for(int k = 0; k < 6; k++) cpu_update_clocks[index][k] = d_cpu[index].update_clocks * k;
 		return index;
 	}
+	/*!
+	  @brief Remove CPU from EVENT MANAGER.
+	  @param device Device pointer requesting to remove.
+	  @param num  CPU number requesting to remove.
+	  @return true if success.
+	  @note You can't remove CPU #0, because this is base of scheduling.
+	  @note You should notify both num and device as same device.
+	*/
 	bool remove_context_cpu(DEVICE* device, int num)
 	{
 		if(num <= 0) return false; // Number one must not be removed.
@@ -304,6 +379,12 @@ public:
 		}
 		return true;
 	}
+	/*!
+	  @brief Set CPU clocks for not primary CPU.
+	  @param device Device pointer expect to set.
+	  @param clocks expect CPU clock by Hz.
+	  @note For CPU #0, this don't effect to.
+	*/
 	void set_secondary_cpu_clock(DEVICE* device, uint32_t clocks)
 	{
 		// XXX: primary cpu clock should not be changed
@@ -318,11 +399,20 @@ public:
 			}
 		}
 	}
+	/*!
+	  @brief Add device to sound source (to mix).
+	  @param device Device pointer expect to add.
+	  @note You can register devices less than MAX_SOUND.
+	*/
 	void set_context_sound(DEVICE* device)
 	{
 		assert(dcount_sound < MAX_SOUND);
 		d_sound[dcount_sound++] = device;
 	}
+	/*!
+	  @brief Check frame skippable.
+	  @return true if avalable to skip.
+	*/
 	bool is_frame_skippable();
 };
 
