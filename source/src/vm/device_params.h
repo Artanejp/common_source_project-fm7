869c18dc5f96d84c03f0a40edf1665c41defe07d
K.Ohta <whatisthis.sowhat@gmail.com>
2022-01-30 15:13:32 +0900

    [VM][DEVICE][DOC] Add details for common SIGNAL definitions.

--- source/src/vm/device_params.h
+++ source/src/vm/device_params.h
@@ -11,33 +11,33 @@
 #define MAX_OUTPUT	16
 
 //!< common signal id
-#define SIG_CPU_IRQ				101
-#define SIG_CPU_FIRQ			102
-#define SIG_CPU_NMI				103
-#define SIG_CPU_BUSREQ			104
-#define SIG_CPU_HALTREQ			105
-#define SIG_CPU_DEBUG			106
-#define SIG_CPU_ADDRESS_DIRTY	107
-#define SIG_CPU_TOTAL_CYCLE_LO	108
-#define SIG_CPU_TOTAL_CYCLE_HI	109
-#define SIG_CPU_WAIT_FACTOR		110
+#define SIG_CPU_IRQ				101	/*!< Make normal interrupt (IRQ) to a target CPU. */
+#define SIG_CPU_FIRQ			102	/*!< Make fast interrupt (FIRQ) to a target CPU. */
+#define SIG_CPU_NMI				103	/*!< Make non maskable  interrupt (NMI) to a target CPU. */
+#define SIG_CPU_BUSREQ			104	/*!< Make bus request (BUSREQ) to a target CPU. Normally non-zero request to release bus (mostly to halt) to target.*/
+#define SIG_CPU_HALTREQ			105	/*!< Make HALT request to a target CPU.Some devices requires halt request separating with BUSREQ. */
+#define SIG_CPU_DEBUG			106	/*!< Make DEBUG pin request to a target */ 
+#define SIG_CPU_ADDRESS_DIRTY	107	/*!< Notify  ARG address of taget made dirty.Will use for cache controlling. */ 
+#define SIG_CPU_TOTAL_CYCLE_LO	108	/*!< Get LOW DWORD of total_icount of a target. */
+#define SIG_CPU_TOTAL_CYCLE_HI	109	/*!< Get HIGH DWORD of total_icount of a target. */
+#define SIG_CPU_WAIT_FACTOR		110	/*!< Set / Get wait factor of a target.This is useful for variable CPU clocks. This value encodes multiply of 65536. */
 
-#define SIG_PRINTER_DATA	201
-#define SIG_PRINTER_STROBE	202
-#define SIG_PRINTER_RESET	203
-#define SIG_PRINTER_BUSY	204
-#define SIG_PRINTER_ACK		205
-#define SIG_PRINTER_SELECT	206
+#define SIG_PRINTER_DATA	201 /*! Read/Write DATA of (pseudo) printer port (normally 8bit or 16bit width) . */ 
+#define SIG_PRINTER_STROBE	202 /*! Read/Write STROBE SIGNAL to (pseudo) printer port. */ 
+#define SIG_PRINTER_RESET	203 /*! Read/Write RESET SIGNAL of (pseudo) printer port. */ 
+#define SIG_PRINTER_BUSY	204 /*! Read/Write BUSY SIGNAL of (pseudo) printer port. */ 
+#define SIG_PRINTER_ACK		205 /*! Read/Write ACKNOWLEDGE SIGNAL of (pseudo) printer port. */ 
+#define SIG_PRINTER_SELECT	206 /*! Read/Write SELECT SIGNAL of (pseudo) printer port. */ 
 
-#define SIG_SCSI_DAT			301
-#define SIG_SCSI_BSY			302
-#define SIG_SCSI_CD				303
-#define SIG_SCSI_IO				304
-#define SIG_SCSI_MSG			305
-#define SIG_SCSI_REQ			306
-#define SIG_SCSI_SEL			307
-#define SIG_SCSI_ATN			308
-#define SIG_SCSI_ACK			309
-#define SIG_SCSI_RST			310
-#define SIG_SCSI_16BIT_BUS		311
-#define SIG_SCSI_CLEAR_QUEUE	312
+#define SIG_SCSI_DAT			301 /*! Read from / Write to a DATA BUS of SCSI bus.Normally 8bit width, but sometimes 16bit width. */ 
+#define SIG_SCSI_BSY			302 /*! Read/Send a BUSY SIGNAL of SCSI bus.*/
+#define SIG_SCSI_CD				303 /*! Read/Send a CD SIGNAL of SCSI bus.*/
+#define SIG_SCSI_IO				304 /*! Read/Send a I/O SIGNAL of SCSI bus.*/
+#define SIG_SCSI_MSG			305 /*! Read/Send a MESSAGE IN SIGNAL of SCSI bus.*/
+#define SIG_SCSI_REQ			306 /*! Read/Send a REQ SIGNAL of SCSI bus.*/
+#define SIG_SCSI_SEL			307 /*! Read/Send a SELECT SIGNAL of SCSI bus.*/
+#define SIG_SCSI_ATN			308 /*! Read/Send a ATTENTION SIGNAL of SCSI bus.*/
+#define SIG_SCSI_ACK			309 /*! Read/Send a ACKNOWLEDGE SIGNAL of SCSI bus.*/
+#define SIG_SCSI_RST			310 /*! Read/Send a RESET SIGNAL of SCSI bus.*/
+#define SIG_SCSI_16BIT_BUS		311 /*! Read/Send a "Notify whether BUS WIDTH is 16bit" SIGNAL of SCSI bus.*/
+#define SIG_SCSI_CLEAR_QUEUE	312 /*! Read/Send a request to clear queue of target SCSI device.*/
