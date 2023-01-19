dabbb26b23bff34460cb35b1a25d9c3980c0f7e5
K.Ohta <whatisthis.sowhat@gmail.com>
2023-01-20 02:03:09 +0900

    [UI][Qt][M23] I forgot to add translations ;_;

---
+++ source/src/qt/machines/m23/m23.ja_JP.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>MachineM23</name>
+    <message>
+        <location filename="MainWindow.cpp" line="32"/>
+        <source>SW1: OS Boot/Debug</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="33"/>
+        <source>SW2: Debug</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="34"/>
+        <source>SW3: Debug</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="35"/>
+        <source>SW4: (Reserved)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="36"/>
+        <source>SW5: (Reserved)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="37"/>
+        <source>SW6: (Reserved)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="38"/>
+        <source>SW7: (Reserved)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="39"/>
+        <source>SW8: Memory Page #0</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="50"/>
+        <source>Floppy Type</source>
+        <translation>フロッピードライブタイプ</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="51"/>
+        <source>FD20-I (3.5inch-1DD)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="52"/>
+        <source>FD20 (5.25inch-1DD)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="53"/>
+        <source>DF44H (8inch-2D)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="57"/>
+        <source>Keyboard Mode</source>
+        <translation>キーボード</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="58"/>
+        <source>ASCII Keyboard</source>
+        <translation>ASCIIキーボード</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="59"/>
+        <source>JIS Keyboard</source>
+        <translation>JISキーボード</translation>
+    </message>
+</context>
+<context>
+    <name>MenuM23</name>
+    <message>
+        <location filename="MainWindow.cpp" line="45"/>
+        <source>Color Monitor</source>
+        <translation>カラーモニター</translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="46"/>
+        <source>Green Monitor</source>
+        <translation>グリーンモニター</translation>
+    </message>
+</context>
+</TS>
