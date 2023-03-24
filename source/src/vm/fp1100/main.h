d8b291802adf85e787293a820f2ec023964b33b0
K.Ohta <whatisthis.sowhat@gmail.com>
2023-03-25 02:39:39 +0900

    [VM][FP1100] Fix FTBFS.

--- source/src/vm/fp1100/main.h
+++ source/src/vm/fp1100/main.h
@@ -22,7 +22,7 @@
 #define SIG_MAIN_COMM	5
 
 namespace FP1100 {
-	
+
 class MAIN : public DEVICE
 {
 private:
@@ -32,7 +32,7 @@ private:
 	DEVICE *d_sub;
 	// to slots
 	DEVICE *d_slot[2][4];
-	
+
 	uint8_t *wbank[16];
 	uint8_t *rbank[16];
 	int wait[16];
@@ -40,19 +40,19 @@ private:
 	uint8_t rdmy[0x1000];
 	uint8_t ram[0x10000];
 	uint8_t rom[0x9000];
-	
+
 	uint8_t comm_data;
 	bool rom_sel;
 	uint8_t slot_sel;
 	uint8_t slot_exp[2];
-	
+
 	uint8_t intr_mask;
 	uint8_t intr_request;
 	uint8_t intr_in_service;
-	
+
 	void update_memory_map();
 	void update_intr();
-	
+
 public:
 	MAIN(VM_TEMPLATE* parent_vm, EMU_TEMPLATE* parent_emu) : DEVICE(parent_vm, parent_emu)
 	{
@@ -60,28 +60,24 @@ public:
 		set_device_name(_T("Main CPU Bus"));
 	}
 	~MAIN() {}
-	
+
 	// common functions
 	void initialize();
 	void reset();
 	void __FASTCALL write_data8(uint32_t addr, uint32_t data);
 	uint32_t __FASTCALL read_data8(uint32_t addr);
-#ifdef Z80_MEMORY_WAIT
 	void __FASTCALL write_data8w(uint32_t addr, uint32_t data, int *wait);
 	uint32_t __FASTCALL read_data8w(uint32_t addr, int *wait);
-#endif
 	void __FASTCALL write_io8(uint32_t addr, uint32_t data);
 	uint32_t __FASTCALL read_io8(uint32_t addr);
-#ifdef Z80_IO_WAIT
 	void __FASTCALL write_io8w(uint32_t addr, uint32_t data, int *wait);
 	uint32_t __FASTCALL read_io8w(uint32_t addr, int *wait);
-#endif
 	void __FASTCALL write_signal(int id, uint32_t data, uint32_t mask);
 	uint32_t get_intr_ack();
 	void notify_intr_reti();
 	void notify_intr_ei();
 	bool process_state(FILEIO* state_fio, bool loading);
-	
+
 	// unique functions
 	void set_context_cpu(DEVICE *device)
 	{
