0076e91d55303f63c626d689262443cb6b207df2
K.Ohta <whatisthis.sowhat@gmail.com>
2023-05-30 17:00:21 +0900

    [General][EMACS] modify .dir-locals.el .

--- .dir-locals.el
+++ .dir-locals.el
@@ -33,4 +33,13 @@
 ;	.
 ;	font-lock-type-face
 ;    ))
-; )
+;
+;)
+
+;(require 'company)
+;(company-mode)
+;(global-company-mode) ; 全バッファで有効にする 
+;(setq company-idle-delay 0) ; デフォルトは0.5
+;(setq company-minimum-prefix-length 2) ; デフォルトは4
+;(setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
+
