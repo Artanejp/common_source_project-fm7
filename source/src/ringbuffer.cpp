c8803b0ebd9a8e05f4c13c873a14c88de3ba7ee8
K.Ohta <whatisthis.sowhat@gmail.com>
2023-08-21 05:10:32 +0900

    [COMMON][RINGBUFFER] Fix pointer handling.

--- source/src/ringbuffer.cpp
+++ source/src/ringbuffer.cpp
@@ -12,27 +12,25 @@
 
 RINGBUFFER::RINGBUFFER(int s, int empty_warn, int fill_warn) : FIFO(s, empty_warn, fill_warn)
 {
-	
+
 }
 
 void RINGBUFFER::write(int val, bool *p_fill_warn)
 {
 	buf[wpt++] = val;
-	if(wpt >= size) {
+	__UNLIKELY_IF(wpt >= size) {
 		wpt = 0;
 	}
 	cnt++;
-	if(cnt >= size) {
+	__UNLIKELY_IF(cnt >= size) {
 		cnt = size;
-		rpt = wpt - 1;
-		if(rpt < 0) rpt = size - 1;
 	}
-	if(fill_warn_val < cnt) {
+	__UNLIKELY_IF(fill_warn_val < cnt) {
 		fill_warn_flag = true;
 	} else {
 		fill_warn_flag = false;
 	}
-	if(p_fill_warn != nullptr) {
+	__UNLIKELY_IF(p_fill_warn != nullptr) {
 		*p_fill_warn = fill_warn_flag;
 	}
 }
@@ -40,71 +38,55 @@ void RINGBUFFER::write(int val, bool *p_fill_warn)
 int RINGBUFFER::read(bool *p_empty_warn)
 {
 	int val = 0;
-	if(cnt) {
+	__LIKELY_IF(cnt > 0) {
 		val = buf[rpt++];
-		if(rpt >= size) {
+		__UNLIKELY_IF(rpt >= size) {
 			rpt = 0;
 		}
 		cnt--;
-		if(cnt <= 0) {
+		__UNLIKELY_IF(cnt <= 0) {
 			cnt = 0;
-			wpt = rpt;
+			wpt = 0;
+			rpt = 0;
 		}
-		if(empty_warn_val > cnt) {
+		__UNLIKELY_IF(empty_warn_val > cnt) {
 			empty_warn_flag = true;
 		} else {
 			empty_warn_flag = false;
 		}
-		if(p_empty_warn != nullptr) {
-			*p_empty_warn = empty_warn_flag;
-		}
+	}
+	__UNLIKELY_IF(p_empty_warn != nullptr) {
+		*p_empty_warn = empty_warn_flag;
 	}
 	return val;
 }
 
 int RINGBUFFER::read_not_remove(int pt, bool *p_empty_warn)
 {
-	if(pt >= 0 && pt < size) {
+	__UNLIKELY_IF(p_empty_warn != nullptr) {
+		*p_empty_warn = empty_warn_flag;
+	}
+	__LIKELY_IF(pt >= 0 && pt < size) {
 		pt += rpt;
-		if(pt >= size) {
+		__UNLIKELY_IF(pt >= size) {
 			pt -= size;
 		}
-		if(empty_warn_val > cnt) {
-			empty_warn_flag = true;
-		} else {
-			empty_warn_flag = false;
-		}
-		if(p_empty_warn != nullptr) {
-			*p_empty_warn = empty_warn_flag;
-		}
 		return buf[pt];
-	}
-	if(p_empty_warn != nullptr) {
-		*p_empty_warn = false;
 	}
 	return 0;
 }
 
 void RINGBUFFER::write_not_push(int pt, int d, bool *p_fill_warn)
 {
-	if(pt >= 0 && pt < size) {
+	__UNLIKELY_IF(p_fill_warn != nullptr) {
+		*p_fill_warn = fill_warn_flag;
+	}
+	__LIKELY_IF(pt >= 0 && pt < size) {
 		pt += wpt;
-		if(pt >= size) {
+		__UNLIKELY_IF(pt >= size) {
 			pt -= size;
 		}
 		buf[pt] = d;
-		if(fill_warn_val < cnt) {
-			fill_warn_flag = true;
-		} else {
-			fill_warn_flag = false;
-		}
-		if(p_fill_warn != nullptr) {
-			*p_fill_warn = fill_warn_flag;
-		}
 		return;
 	}
-	if(p_fill_warn != nullptr) {
-		*p_fill_warn = false;
-	}
 }
-
