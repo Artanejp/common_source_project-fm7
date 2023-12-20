519ed7639441506fdf8c7cd8d5dd5d084c49cda9
K.Ohta <whatisthis.sowhat@gmail.com>
2023-12-21 05:59:57 +0900

    [COMMON][UTIL_SOUND] Make apply_volume() be simple.

--- source/src/types/util_sound.h
+++ source/src/types/util_sound.h
@@ -22,22 +22,12 @@ inline int __FASTCALL decibel_to_volume(int decibel)
 
 inline int32_t __FASTCALL apply_volume(int32_t sample, int volume)
 {
-//	int64_t output;
-	int32_t output;
-	if(sample < 0) {
-		output = -sample;
-		output *= volume;
-		output >>= 10;
-		output = -output;
-	} else {
-		output = sample;
-		output *= volume;
-		output >>= 10;
-	}
+	//int64_t output = ((uint64_t)sample) * ((uint64_t)volume);
+	int64_t output = ((int64_t)sample) * volume;
+	output >>= 10;
 	return output;
 }
 
 // High pass filter and Low pass filter.
 void DLL_PREFIX calc_high_pass_filter(int32_t* dst, int32_t* src, int sample_freq, int hpf_freq, int samples, double quality = 1.0, bool is_add = true);
 void DLL_PREFIX calc_low_pass_filter(int32_t* dst, int32_t* src, int sample_freq, int lpf_freq, int samples, double quality = 1.0, bool is_add = true);
-
