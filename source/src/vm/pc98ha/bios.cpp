f7e8d6761db2fbcbf336a742f9d813e13ec88bc8
K.Ohta <whatisthis.sowhat@gmail.com>
2024-09-10 16:11:51 +0900

    [VM][PC98HA][BIOS] Make bios_int_i86() inherits DEVICE:: .
    - This fixes some issues with calling BIOS.

--- source/src/vm/pc98ha/bios.cpp
+++ source/src/vm/pc98ha/bios.cpp
@@ -18,16 +18,29 @@ namespace PC98HA {
 #define AL	regs8[0]
 #define AH	regs8[1]
 
-bool BIOS::bios_int_i86(int intnum, uint16_t regs[], const uint16_t sregs[], int32_t* ZeroFlag, int32_t* CarryFlag)
+#define CALC_CYCLES(a,b,e)									  \
+	{														  \
+		__LIKELY_IF(a != NULL) {							  \
+			*a -= e;										  \
+		}													  \
+		__LIKELY_IF(b != NULL) {							  \
+			*b += (uint64_t)e;								  \
+		}													  \
+	}
+
+	
+bool BIOS::bios_int_i86(int intnum, uint16_t regs[], const uint16_t sregs[], int32_t* ZeroFlag, int32_t* CarryFlag, int* cycles, uint64_t* total_cycles)
 {
 	static const int check_cmds[16] = {1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0};
 	uint8_t *regs8 = (uint8_t *)regs;
-	
+	int elapsed_cycles = 200; // TBD.
 	if(intnum == 0x1b && check_cmds[AH & 0x0f]) {
 		switch(AL & 0xf0) {
 		case 0x10:
 		case 0x70:
 			// 2DD
+			// ToDo: This will be FIX cycle value.
+			CALC_CYCLES(cycles, total_cycles, elapsed_cycles); 
 			if(d_fdc->is_disk_inserted(AL & 0x03) && d_fdc->get_media_type(AL & 0x03) != MEDIA_TYPE_2DD) {
 				AH = 0xe0;
 				*CarryFlag = 1;
@@ -37,6 +50,8 @@ bool BIOS::bios_int_i86(int intnum, uint16_t regs[], const uint16_t sregs[], int
 		case 0x90:
 		case 0xf0:
 			// 2HD
+			// ToDo: This will be FIX cycle value.
+			CALC_CYCLES(cycles, total_cycles, elapsed_cycles); 
 			if(d_fdc->is_disk_inserted(AL & 0x03) && d_fdc->get_media_type(AL & 0x03) != MEDIA_TYPE_2HD) {
 				AH = 0xe0;
 				*CarryFlag = 1;
