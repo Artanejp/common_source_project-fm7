2d6b149c2588f3c7c0adb4f0e544e44a24927972
K.Ohta <whatisthis.sowhat@gmail.com>
2026-02-18 05:20:30 +0900

    [VM][EVENT][BUG] Fix crash when try to cancel NULL device.

--- source/src/vm/event.cpp
+++ source/src/vm/event.cpp
@@ -587,11 +587,11 @@ void EVENT::cancel_event(DEVICE* device, int register_id)
 	// cancel registered event
 	__LIKELY_IF(0 <= register_id && register_id < MAX_EVENT) {
 		event_t *event_handle = &event[register_id];
-		__UNLIKELY_IF(device != NULL && device != event_handle->device) {
+		__UNLIKELY_IF(device != NULL && (device != event_handle->device)) {
 			this->out_debug_log(_T("EVENT: device (name=%s, id=%d) tries to cancel event %d that is not its own (owned by (name=%s id=%d))!!!\n"), device->this_device_name, device->this_device_id,
 								register_id,
-								event_handle->device->this_device_name,
-								event_handle->device->this_device_id);
+								(event_handle->device != NULL) ? event_handle->device->this_device_name : _T("NULL DEVICE"),
+								(event_handle->device != NULL) ? event_handle->device->this_device_id : -1);
 			return;
 		}
 		__LIKELY_IF(event_handle->active) {
